c844429ffe51de621e9c9bcd1a3dbd9b92df9ca9
Ori Granot <oriorigranot@gmail.com>
2020-03-21 19:34:37 +0200

    Add shortner service

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-// * View engine and settings
 app.set('view engine', 'pug')
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
-// * Routes
 const indexRoutes = require('./routes/index')
 const apiRoutes = require('./routes/api')
 
 app.use('/', indexRoutes)
 app.use('/api', apiRoutes)
 
+// Declare Globals
+global.URL_DICT = {}
 
 app.listen(process.env.APP_PORT, () => console.log(`App listening on port ${process.env.APP_PORT}!`))
