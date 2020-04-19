22b9eb5ab1c23856b7463c62c8d270f2fcacb340
Ori Granot <oriorigranot@gmail.com>
2020-04-19 17:18:34 +0300

    Returns existing shorter url option

--- services/shortner.js
+++ services/shortner.js
@@ -3,27 +3,39 @@
 /**
  * Returns a random 5 characters url.
  */
-const generateShortUrl = () => { 
-    return Math.random().toString(36).substring(2,7);
+const generateShortUrl = () => {
+    return Math.random().toString(36).substring(2, 7);
 }
 
 /**
  * Check if the short url is not in use.
- * @param {String} shortUrl - The shorten url.
+ * @param {String} shortUrl The shorten url.
+ * @returns {Boolean} Is the shortner url taken
  */
-const isShortUrlAvailable = (shortUrl) => { 
+const isShortUrlAvailable = (shortUrl) => {
     return (global.URL_DICT[shortUrl] === undefined)
 }
 
 /**
- * Add the short url to the server routes.
- * @param {String} originalUrl - The original url.
- * @param {String} shortUrl - The shorten url.
+ * Return the shortner url of the specific url.
+ * @param {String} originalUrl The original url.
+ * @returns {String} Returns the key or null
  */
-const addUrl = (originalUrl, shortUrl) => { 
+const getShortUrl = (originalUrl) => {
+    let urlIndex = Object.values(global.URL_DICT).indexOf(originalUrl);
+    return urlIndex !== -1 ? Object.keys(global.URL_DICT)[urlIndex] : null;
+}
+
+/**
+ * Add the short url to the server routes.
+ * @param {String} originalUrl The original url.
+ * @param {String} shortUrl The shorten url.
+ */
+const addUrl = (originalUrl, shortUrl) => {
     global.URL_DICT[shortUrl] = originalUrl;
 }
 
 module.exports.generateShortUrl = generateShortUrl;
 module.exports.isShortUrlAvailable = isShortUrlAvailable;
 module.exports.addUrl = addUrl;
+module.exports.getShortUrl = getShortUrl;
