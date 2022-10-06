0b63f307450aa6403c2c92b3a1541d50bf2203cf
Guillem Padilla <guillem.padilla@companial.com>
2022-10-06 14:06:17 +0300

    Load modules on app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ShortenerModule } from './modules';
+import { AppRouterModule } from './routes';
 
 @Module({
-  imports: [],
+  imports: [
+    ShortenerModule,
+    AppRouterModule
+  ],
   controllers: [AppController],
   providers: [AppService],
   
