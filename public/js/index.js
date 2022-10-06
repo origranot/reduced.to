f0bedc3a469d6afa7b8283ca6e888dc949b7689e
canaryGrapher <yasharyan307@outlook.com>
2022-10-06 22:06:22 +0530

    Code Organization: Use JSDoc

--- public/js/index.js
+++ public/js/index.js
@@ -4,84 +4,95 @@
 let invalidUrl = false;
 
 /**
- * Handles shortener button click.
+ * @function handleShortenerClick
+ * @description Handles the click event on the shorten button and simulates the UI
  */
 const handleShortenerClick = async () => {
-	const result = document.getElementById("result");
-	const loader = document.getElementById("loading");
-	const urlInput = document.getElementById("urlInput");
+    const result = document.getElementById("result");
+    const loader = document.getElementById("loading");
+    const urlInput = document.getElementById("urlInput");
 
-	loader.style.display = "block";
-	result.style.display = "none";
+    loader.style.display = "block";
+    result.style.display = "none";
 
-	invalidUrl = false
+    invalidUrl = false
 
-	const shortenInfo = await getShortenUrl(urlInput.value);
+    const shortenInfo = await getShortenUrl(urlInput.value);
 
-	// Remove the loader from the screen
-	loader.style.display = "none";
-	result.style.display = "block";
+    // Remove the loader from the screen
+    loader.style.display = "none";
+    result.style.display = "block";
 
-	if (shortenInfo === null) {
-		result.textContent = 'This url is invalid..';
-		invalidUrl = true;
-		return;
-	}
+    if (shortenInfo === null) {
+        result.textContent = 'This url is invalid..';
+        invalidUrl = true;
+        return;
+    }
 
-	const { newUrl } = shortenInfo;
-	result.querySelector('#text').textContent = window.location.href + newUrl;
-	result.querySelector('#action').classList.replace('d-none', 'd-block');
+    const { newUrl } = shortenInfo;
+    // result.querySelector('#text').textContent = window.location.href + newUrl;
+    document.getElementById("urlResult").value = window.location.href + newUrl;
+    result.querySelector('#action').classList.replace('d-none', 'd-flex');
 
-	copyUrl()
+    copyUrl()
 };
 
 /**
- * Returns the shorter link from the server.
- * @param {String} originalUrl - The original url we want to shorten.
+ * @function getShortenUrl
+ * @description Shorten the URL.
+ * @param {String} originalUrl - The original URL that needs to be shortened
  */
 const getShortenUrl = async (originalUrl) => {
-	let result;
-	try {
-		result = await axios.post('/api/shortener', { originalUrl });
-	} catch (err) {
-		return null;
-	}
-	return result.data;
+    let result;
+    try {
+        result = await axios.post('/api/shortener', { originalUrl });
+    } catch (err) {
+        return null;
+    }
+    return result.data;
 };
+
 
 /**
- * Copy link to clipboard.
+ * @function copyUrl
+ * @description Copies the shortened link to the clipboard.
  */
-
 const copyUrl = () => {
-	if (invalidUrl) {
-		return;
-	}
-
-	const result = document.querySelector("#result #text");
-	navigator.clipboard.writeText(result.textContent);
-	toastAlert();
+    if (invalidUrl) {
+        return;
+    }
+    const result = document.querySelector("#result #text");
+    navigator.clipboard.writeText(result.textContent);
+    toastAlert();
 };
 
+
+/**
+ * @function toastAlert
+ * @description Toast alert when URL is copied.
+ * @param {Number} timeoutInMiliseconds - The time in miliseconds to show the toast.
+ */
 const toastAlert = (timeoutInMiliseconds = 2000) => {
-	const urlAlert = document.getElementById("urlAlert");
+    const urlAlert = document.getElementById("urlAlert");
+    urlAlert.classList.add('fade-in');
+    urlAlert.classList.remove('collapse');
 
-	urlAlert.classList.add('fade-in');
-	urlAlert.classList.remove('collapse');
+    setTimeout(() => {
+        urlAlert.classList.remove('fade-in');
+        urlAlert.classList.add('fade-out');
 
-	setTimeout(() => {
-		urlAlert.classList.remove('fade-in');
-		urlAlert.classList.add('fade-out');
-
-		setTimeout(() => {
-			urlAlert.classList.add('collapse');
-			urlAlert.classList.remove('fade-out');
-		}, 500);
-	}, timeoutInMiliseconds);
+        setTimeout(() => {
+            urlAlert.classList.add('collapse');
+            urlAlert.classList.remove('fade-out');
+        }, 500);
+    }, timeoutInMiliseconds);
 }
 
-// Open link in a new window/tab.
+/**
+ * @function openLink
+ * @description Opens the shortened link in a new tab/window.
+ */
 const openLink = () => {
-	const text = document.querySelector('#result #text').textContent;
-	window.open(text, '_blank');
+    const text = document.getElementById("urlResult").value;
+    window.open(text, '_blank');
 };
