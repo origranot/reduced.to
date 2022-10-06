cfa40ff89410960bfe955cb0b7e99ef3c2c3ddac
Guillem Padilla <guillem.padilla@companial.com>
2022-10-06 14:37:44 +0300

    Implement shortener service methods

--- src/modules/shortener/services/shortener.service.ts
+++ src/modules/shortener/services/shortener.service.ts
@@ -2,7 +2,39 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ShortenerService {
-  shortUrl() {
-    return 'Handler not implemented'
-  }
+  /**
+   * Return the shortener url of the specific url.
+   * @param {String} originalUrl The original url.
+   * @returns {String} Returns the key or null
+   */
+  getShortUrl(originalUrl: string): string {
+    const urlIndex = Object.values(global.URL_DICT).indexOf(originalUrl);
+    return urlIndex !== -1 ? Object.keys(global.URL_DICT)[urlIndex] : null;
+  };
+
+  /**
+ * Generating the short url
+ * @returns {String} Returns a random 5 characters url
+ */
+  generateShortUrl = (): string => {
+    return Math.random().toString(36).substring(2, 7);
+  };
+
+  /**
+   * Check if the short url is not in use.
+   * @param {String} shortUrl The shorten url.
+   * @returns {Boolean} Is the shortener url taken
+   */
+  isShortUrlAvailable = (shortUrl: string): boolean => {
+    return global.URL_DICT[shortUrl] === undefined;
+  };
+
+  /**
+ * Add the short url to the server routes.
+ * @param {String} originalUrl The original url.
+ * @param {String} shortUrl The shorten url.
+ */
+  addUrl = (originalUrl: string, shortUrl: string) => {
+    global.URL_DICT[shortUrl] = originalUrl;
+  };
 }
