921fda5b762bc29243af0d5c4a5df12bca11a4f0
Guillem Padilla <guillem.padilla@companial.com>
2022-10-06 14:43:21 +0300

    Remove unused imports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,12 @@
-import { Controller, Get, Post, Render, VERSION_NEUTRAL } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Render, VERSION_NEUTRAL } from '@nestjs/common';
 
 @Controller({
   version: VERSION_NEUTRAL
 })
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
   @Render('index')
   root() {
     return;
-  }
-
-  @Post()
-  requestHandle() {
-    this.appService.getHello();
   }
 }
