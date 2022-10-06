ff7a179f5994474d86e184f2a27227711c28be86
AkhilJ321 <jamwalakhil936@gmail.com>
2022-10-07 00:15:01 +0530

    added a toast for 'link has been copied'

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * Handles shortener button click.
  */
 const handleShortenerClick = async () => {
-	let originalUrl = document.getElementById('urlInput').value;
-	let shortenInfo = await getShortenUrl(originalUrl);
+  let originalUrl = document.getElementById("urlInput").value;
+  let shortenInfo = await getShortenUrl(originalUrl);
 
-	if (shortenInfo === null) {
-		document.getElementById('result').textContent = 'This url is invalid..';
-		return;
-	}
+  if (shortenInfo === null) {
+    document.getElementById("result").textContent = "This url is invalid..";
+    return;
+  }
 
-	let { newUrl } = shortenInfo;
-	document.getElementById('result').textContent = window.location.href + newUrl;
-	document.getElementById('urlAlert').classList.add('collapse');
+  let { newUrl } = shortenInfo;
+  document.getElementById("result").textContent = window.location.href + newUrl;
+  //   document.getElementById("urlAlert").classList.add("collapse");
 };
 
 /**
@@ -22,23 +22,52 @@ const handleShortenerClick = async () => {
  * @param {String} originalUrl - The original url we want to shorten.
  */
 const getShortenUrl = async (originalUrl) => {
-	let result;
-	try {
-		result = await axios.post('/api/shortener', {
-			originalUrl,
-		});
-	} catch (err) {
-		return null;
-	}
-	return result.data;
+  let result;
+  try {
+    result = await axios.post("/api/shortener", {
+      originalUrl,
+    });
+  } catch (err) {
+    return null;
+  }
+  return result.data;
 };
+const Toast = {
+  init() {
+    this.hideTimeout = null;
+
+    this.el = document.createElement("div");
+    this.el.className = "toast";
+    document.body.appendChild(this.el);
+  },
+
+  show(message, state) {
+    clearTimeout(this.hideTimeout);
+
+    this.el.textContent = message;
+    this.el.className = "toast toast--visible";
+
+    if (state) {
+      this.el.classList.add(`toast--${state}`);
+    }
+
+    this.hideTimeout = setTimeout(() => {
+      this.el.classList.remove("toast--visible");
+    }, 3000);
+  },
+};
+
+document.addEventListener("DOMContentLoaded", () => Toast.init());
 
 /**
  * Copy link to clipboard.
  * @param {HTMLElement} htmlElement - HTML Element containing the short url.
  */
 const copyUrl = async (htmlElement) => {
-	navigator.clipboard.writeText(htmlElement.innerHTML);
+  await navigator.clipboard.writeText(htmlElement.innerHTML);
+  if (htmlElement.innerHTML !== "This url is invalid..") {
+    Toast.show("Your link has been copied to the clipboard!", "success");
+  }
 
-	document.getElementById('urlAlert').classList.remove('collapse');
+  // document.getElementById('urlAlert').classList.remove('collapse');
 };
