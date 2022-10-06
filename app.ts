9054acb4a0786a73f36d9266804e0b4d0cca9217
orig <48911144+origranot@users.noreply.github.com>
2022-10-06 11:08:03 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 const app = express();
 
-const APP_PORT = 3000;
+const APP_PORT = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
 
