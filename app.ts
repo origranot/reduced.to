0937426b3d377538cd3f697ee50a6433641dd11a
Jarix <rija.nifaliana@gmail.com>
2022-10-06 10:15:16 +0300

    ⚡️ Typecript > Refac app

---
+++ app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+
+const APP_PORT = 3000;
+
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+
+const indexRoutes = require("./routes/index");
+const apiRoutes = require("./routes/api");
+
+app.use("/", indexRoutes);
+app.use("/api", apiRoutes);
+
+// Declare Globals
+//@ts-ignore
+global.URL_DICT = {};
+
+app.listen(APP_PORT, () => console.log(`App listening on port ${APP_PORT}!`));
