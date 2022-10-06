480633551684badf13c3ce12e6e1ab0065d09fc0
Guillem Padilla <guillem.padilla@companial.com>
2022-10-06 15:53:52 +0300

    Fix URL redirect

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get, Render, VERSION_NEUTRAL } from '@nestjs/common';
+import { Controller, Get, Param, Redirect, Render, VERSION_NEUTRAL } from '@nestjs/common';
 
 @Controller({
   version: VERSION_NEUTRAL
 })
 export class AppController {
+  constructor() { }
+
   @Get()
   @Render('index')
   root() {
+    return;
+  }
+
+  @Get(':path')
+  @Redirect('http://localhost:3000', 301)
+  redirect(@Param() params: { path: string }) {
+	  const originalUrl = global.URL_DICT[params.path];
+    if (originalUrl !== undefined) {
+      return { url: originalUrl }
+    }
     return;
   }
 }
