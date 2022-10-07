e71c760ce426e30907c505a40f827ec9124cae43
Guillem Padilla <guillem.martin.p@gmail.com>
2022-10-07 19:02:47 +0300

    Add main page (without scripts)

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,139 +1,67 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { Link } from '@builder.io/qwik-city';
+import animations from '../assets/css/animations.css?inline';
+import loader from '../assets/css/loader.css?inline';
+import styles from './index.css?inline';
 
 export default component$(() => {
+  useStylesScoped$(animations)
+  useStylesScoped$(styles)
+  useStylesScoped$(loader)
+
   return (
-    <div>
-      <h1>
-        Welcome to Qwik <span class="lightning">⚡️</span>
+    <div class="container">
+      <h1 class="p-3 text-light font-weight-bold">
+        URL Shortener
       </h1>
-
-      <ul>
-        <li>
-          Check out the <code>src/routes</code> directory to get started.
-        </li>
-        <li>
-          Add integrations with <code>npm run qwik add</code>.
-        </li>
-        <li>
-          More info about development in <code>README.md</code>
-        </li>
-      </ul>
-
-      <h2>Commands</h2>
-
-      <table class="commands">
-        <tr>
-          <td>
-            <code>npm run dev</code>
-          </td>
-          <td>Start the dev server and watch for changes.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run preview</code>
-          </td>
-          <td>Production build and start preview server.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run build</code>
-          </td>
-          <td>Production build.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add</code>
-          </td>
-          <td>Select an integration to add.</td>
-        </tr>
-      </table>
-
-      <h2>Add Integrations</h2>
-
-      <table class="commands">
-        <tr>
-          <td>
-            <code>npm run qwik add cloudflare-pages</code>
-          </td>
-          <td>
-            <a href="https://developers.cloudflare.com/pages" target="_blank">
-              Cloudflare Pages Server
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add express</code>
-          </td>
-          <td>
-            <a href="https://expressjs.com/" target="_blank">
-              Nodejs Express Server
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add netlify-edge</code>
-          </td>
-          <td>
-            <a href="https://docs.netlify.com/" target="_blank">
-              Netlify Edge Functions
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add static-node</code>
-          </td>
-          <td>
-            <a
-              href="https://qwik.builder.io/qwikcity/static-site-generation/overview/"
-              target="_blank"
-            >
-              Static Site Generation (SSG)
-            </a>
-          </td>
-        </tr>
-      </table>
-
-      <h2>Community</h2>
-
-      <ul>
-        <li>
-          <span>Questions or just want to say hi? </span>
-          <a href="https://qwik.builder.io/chat" target="_blank">
-            Chat on discord!
-          </a>
-        </li>
-        <li>
-          <span>Follow </span>
-          <a href="https://twitter.com/QwikDev" target="_blank">
-            @QwikDev
-          </a>
-          <span> on Twitter</span>
-        </li>
-        <li>
-          <span>Open issues and contribute on </span>
-          <a href="https://github.com/BuilderIO/qwik" target="_blank">
-            Github
-          </a>
-        </li>
-        <li>
-          <span>Watch </span>
-          <a href="https://qwik.builder.io/media/" target="_blank">
-            Presentations, Podcasts, Videos, etc.
-          </a>
-        </li>
-      </ul>
-      <Link class="mindblow" href="/flower">
-        Blow my mind 🤯
-      </Link>
+      <div class="alert alert-primary" role="alert">
+        Add your very long <b>URL</b> in the input bellow and click on the button to make it shorter
+      </div>
+      <div class="input-group mb-3">
+        <input type="text" id="urlInput" class="border-primary text-light bg-dark form-control" placeholder="Very long url..." aria-label="url" aria-describedby="shortenerBtn" />
+        <div class="input-group-append">
+          <button type="button" id="shortenerBtn" class="btn btn-primary">Shorten URL</button>
+        </div>
+      </div>
+      <div id="loading" class="fade-in">
+        <div class="lds-dual-ring"></div>
+      </div>
+      <div id="result" class="text-light">
+          <p id="error" className="text-light fade-in"></p>
+          <p id="text" className="text-light fade-in"></p>
+          <div id="action" className="d-none flex">
+            <button type="button" className="btn btn-dark mr-1">
+              <i className="bi bi-clipboard"></i>
+            </button>
+            <button type="button" className="btn btn-dark">
+              <i className="bi bi-box-arrow-up-right"></i>
+            </button>
+          </div>
+      </div>
+      <div id="urlAlert" className="alert alert-success collapse" role="alert">
+        Link has been copied to the clipboard
+      </div>
+      <div className="waves-div">
+        <svg class="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+          <defs>
+            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path>
+          </defs>
+          <g class="parallax">
+            {/* @ts-ignore */}
+            <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7"></use>
+            {/* @ts-ignore */}
+            <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)"></use>
+            {/* @ts-ignore */}
+            <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)"></use>
+            {/* @ts-ignore */}
+            <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff"></use>
+          </g>
+          </svg>
+      </div>
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'URL Shortener',
 };
