3aedac0ce9fa001f98459231a773f6c111671d7f
Guillem Padilla <guillem.martin.p@gmail.com>
2022-10-07 14:28:57 +0300

    Create Qwik app (Frontend)

---
+++ frontend/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig(() => {
+  return {
+    ssr: { target: "node", format: "cjs" },
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+  };
+});
