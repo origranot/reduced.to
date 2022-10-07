37903d39c242b1b9d233d1317938fe65665f5fd9
Dev Code <hellodev_code@outlook.com>
2022-10-07 14:46:17 +0530

    added toggle dark mode btn work with every DOM element

---
+++ public/js/togglebtn.js
@@ -0,0 +1,21 @@
+// Making the toggle button work
+const toggle = document.getElementById(`toggle`);
+const body = document.querySelector(`body`);
+const urlTxt = document.querySelector(`.upper-body-txt`);
+const urlInput = document.getElementById(`urlInput`);
+const indicator = document.querySelector(`.indicator`);
+const result = document.getElementById(`result`);
+const darkParallax = document.querySelector(`.dark-parallax`);
+const error = document.getElementById(`error`);
+const text = document.getElementById(`text`);
+const toggleMode = function () {
+  toggle.classList.toggle(`active`);
+  body.classList.toggle(`active`);
+  urlTxt.classList.toggle(`active`);
+  urlInput.classList.toggle(`active`);
+  indicator.classList.toggle(`active`);
+  result.classList.toggle(`active`);
+  darkParallax.classList.toggle(`active`);
+  error.classList.toggle(`active`);
+  text.classList.toggle(`active`);
+};
