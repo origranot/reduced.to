2450b75e6f0e0d588cbe2bd6928d08c51f408744
Guillem Padilla <guillem.martin.p@gmail.com>
2022-10-07 14:30:57 +0300

    Update main.ts to use static files generated by Qwik

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,8 +16,6 @@ async function bootstrap() {
   });
 
   app.useStaticAssets(join(__dirname, 'public'));
-  app.setBaseViewsDir(join(__dirname, 'views'));
-  app.setViewEngine('pug');
   
   // Declare Globals
   global.URL_DICT = {}
