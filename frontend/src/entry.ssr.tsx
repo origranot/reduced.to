ce32a71854fae59d1aee8230726b8b2589e61ddb
Guillem Padilla <guillem.martin.p@gmail.com>
2022-10-10 23:30:10 +0300

    Add @builder.io/qwik-react

--- frontend/src/entry.ssr.tsx
+++ frontend/src/entry.ssr.tsx
@@ -1,16 +1,5 @@
-/**
- * WHAT IS THIS FILE?
- *
- * SSR entry point, in all cases the application is render outside the browser, this
- * entry point will be the common one.
- *
- * - Server (express, cloudflare...)
- * - npm run start
- * - npm run preview
- * - npm run build
- *
- */
-import { renderToStream, RenderToStreamOptions } from '@builder.io/qwik/server';
+import type { RenderToStreamOptions } from '@builder.io/qwik/server';
+import { renderToStream } from '@builder.io/qwik-react';
 import { manifest } from '@qwik-client-manifest';
 import Root from './root';
 
@@ -18,12 +7,5 @@ export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
     manifest,
     ...opts,
-    prefetchStrategy: {
-      implementation: {
-        linkInsert: null,
-        workerFetchInsert: null,
-        prefetchEvent: 'always',
-      },
-    },
   });
 }
