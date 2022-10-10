ec1b275d9092397ebe646f6dd564aa22d9f42ceb
Guillem Padilla <guillem.martin.p@gmail.com>
2022-10-10 23:30:10 +0300

    Add @builder.io/qwik-react

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { qwikReact } from "@builder.io/qwik-react";
 
 export default defineConfig(() => {
   return {
     ssr: { target: "node", format: "cjs" },
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), qwikReact()],
   };
 });
