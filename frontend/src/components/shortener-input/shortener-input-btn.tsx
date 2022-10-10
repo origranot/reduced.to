359b6f33bfe0f1c86277db527415a07bfad32595
Guillem Padilla <guillem.martin.p@gmail.com>
2022-10-10 16:42:07 +0300

    Create component for input button

---
+++ frontend/src/components/shortener-input/shortener-input-btn.tsx
@@ -0,0 +1,21 @@
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import styles from './shortener-input-btn.css?inline';
+
+export interface ShortenerInputBtnProps {
+    ref: any;
+    onClick$: () => void;
+}
+
+export const ShortenerInputBtn = component$((props: ShortenerInputBtnProps) => {
+    useStylesScoped$(styles)
+
+    return (
+        <button 
+            ref={props.ref}
+            onClick$={props.onClick$}
+            type="button"
+            id="shortenerBtn"
+            class="btn btn-animation"
+        >Shorten URL</button>
+    )
+})
