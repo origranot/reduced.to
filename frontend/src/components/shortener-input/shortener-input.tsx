2965030b35b081a0840da42dd42bbffb2c520128
Yossi Schechter <jschechter@bitbean.com>
2022-10-12 16:45:45 -0400

    centered input on mobile

--- frontend/src/components/shortener-input/shortener-input.tsx
+++ frontend/src/components/shortener-input/shortener-input.tsx
@@ -4,36 +4,35 @@ import { handleShortener } from "./handleShortener";
 import { ShortenerInputBtn } from "./shortener-input-btn";
 
 export interface ShortenerInputProps {
-    ref: any;
-    onKeyUp$: (event: KeyboardEvent) => void;
-    onInput$: (event: InputEvent) => void;
+  ref: any;
+  onKeyUp$: (event: KeyboardEvent) => void;
+  onInput$: (event: InputEvent) => void;
 }
 
 export const ShortenerInput = component$((props: ShortenerInputProps) => {
-    const state: Store = useContext(InputContext) as Store;
-    const shortenerInputBtnRef = useRef();
-    return (
-        <div className="form-control">
-            <div class="input-group mb-3 flex">
-                <input
-                    ref={props.ref}
-                    onKeyUp$={props.onKeyUp$}
-                    onInput$={props.onInput$}
-                    value={state.inputValue}
-                    type="text"
-                    id="urlInput"
-                    class="input input-bordered bg-base-200 flex-auto"
-                    placeholder="Very long url..."
-                    aria-label="url"
-                    aria-describedby="shortenerBtn"
-                />
-                <ShortenerInputBtn
-                    ref={shortenerInputBtnRef}
-                    disabled={state.inputValue.length === 0}
-                    onClick$={() => handleShortener(state)}
-                >
-                </ShortenerInputBtn>
-            </div>
-        </div>
-    )
-})
+  const state: Store = useContext(InputContext) as Store;
+  const shortenerInputBtnRef = useRef();
+  return (
+    <div className="form-control m-3 md:m-0  ">
+      <div class="input-group md:m-3  flex flex-col md:flex-row">
+        <input
+          ref={props.ref}
+          onKeyUp$={props.onKeyUp$}
+          onInput$={props.onInput$}
+          value={state.inputValue}
+          type="text"
+          id="urlInput"
+          class="input input-bordered bg-base-200 flex-auto"
+          placeholder="Very long url..."
+          aria-label="url"
+          aria-describedby="shortenerBtn"
+        />
+        <ShortenerInputBtn
+          ref={shortenerInputBtnRef}
+          disabled={state.inputValue.length === 0}
+          onClick$={() => handleShortener(state)}
+        />
+      </div>
+    </div>
+  );
+});
