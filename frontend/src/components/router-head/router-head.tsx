08fcdbfdd643e7308b376875236b5bfdb44c9cfb
Guillem Padilla Martín <guillem.martin.p@gmail.com>
2022-10-12 21:28:03 +0300

    Enhancement index into components and better UI (#177)
    * Create component for input button
    
    * Add component for shortener input
    
    * Export Qrl functions into exported functions
    
    * Remove btn css
    
    * Update function calls and use components
    
    * Create component for waves
    
    * Add component for github buttons
    
    * Add component for Toast
    
    * Rename toast to Alert
    
    * Install and remove dependencies CSS
    
    * Setup tailwindcss and DaisyUI
    
    * Remove bootstrap CDN
    
    * Set theme to dracula
    
    * Remove body background
    
    * Apply daisy styles
    
    * Add alert component
    
    * Refactor shortener-input component
    
    * Refactor handleShortener
    
    * Refactor index
    
    * Add alert component
    
    * Remove alert.css
    
    * Format
    
    * Update frameworks on README
    
    * Update tailwind link
    
    * Add component for QRCode
    
    * Add task to roadmap

--- frontend/src/components/router-head/router-head.tsx
+++ frontend/src/components/router-head/router-head.tsx
@@ -22,8 +22,6 @@ export const RouterHead = component$(() => {
         href="https://fonts.googleapis.com/css2?family=Poppins&amp;display=swap"
         rel="stylesheet"
       />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.9.1/font/bootstrap-icons.min.css" integrity="sha512-5PV92qsds/16vyYIJo3T/As4m2d8b6oWYfoqV+vtizRB6KhF1F9kYzWzQmsO6T3z3QG2Xdhrx7FQ+5R1LiQdUA==" crossOrigin="anonymous"></link>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"></link>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" integrity="sha512-c42qTSw/wPZ3/5LBzD+Bw5f7bSF2oxou6wEb+I/lqeaKV5FDIfMvvRp772y4jcJLKuGUOpbJMdg/BTl50fJYAw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       <script async defer src="https://buttons.github.io/buttons.js"></script>
       <meta property="og:site_name" content="Qwik" />
