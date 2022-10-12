b8244320e84f47c69151ba35a3235371095e130a
Guillem Padilla <guillem.martin.p@gmail.com>
2022-10-12 06:35:43 +0300

    Refactor shortener-input component

--- frontend/src/components/shortener-input/shortener-input.tsx
+++ frontend/src/components/shortener-input/shortener-input.tsx
@@ -1,36 +1,34 @@
-import { component$, useRef } from "@builder.io/qwik";
-import { handleShortenerClick } from "./handleShortener";
+import { component$, useContext, useRef } from "@builder.io/qwik";
+import { InputContext, Store } from "~/routes";
+import { handleShortener } from "./handleShortener";
 import { ShortenerInputBtn } from "./shortener-input-btn";
 
 export interface ShortenerInputProps {
     ref: any;
-    onKeyUp$: (event: KeyboardEvent) => void;
+    onKeyUp$: ({ store }: any) => void;
 }
 
 export const ShortenerInput = component$((props: ShortenerInputProps) => {
+  const state: Store = useContext(InputContext) as Store;
 
-    const shortenerInputBtnRef = useRef();
-
+  const shortenerInputBtnRef = useRef();
     return (
-    <div class="input-group mb-3">
+    <div class="input-group mb-3 flex">
         <input
             ref={props.ref}
             onKeyUp$={props.onKeyUp$}
             type="text" 
             id="urlInput"
-            class="border-primary text-light bg-dark form-control"
+            class="input input-bordered bg-base-200 flex-auto"
             placeholder="Very long url..."
             aria-label="url"
             aria-describedby="shortenerBtn" 
         />
-        <div class="input-group-append">
             <ShortenerInputBtn
                 ref={shortenerInputBtnRef}
-                onClick$={() => handleShortenerClick()}
+                onClick$={() => handleShortener({state})}
             >
-
             </ShortenerInputBtn>
-        </div>
     </div>
     )
 })
