074e490c827bfeefc106923927fd9d2e1d88ce1d
Ori Granot <oriorigranot@gmail.com>
2022-10-15 23:48:20 +0300

    test: add tests to shortener service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,10 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ShortenerModule } from './shortener/shortener.module';
+import { AppCacheModule } from './cache/cache.module';
 
 @Module({
-  imports: [
-    CacheModule.register({
-      isGlobal: true,
-      ttl: 0, // Unlimited time
-    }),
-    ShortenerModule,
-  ],
+  imports: [AppCacheModule, ShortenerModule],
   controllers: [AppController],
 })
 export class AppModule {}
