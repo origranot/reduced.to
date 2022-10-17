2456753c5a2b47fd4f069fa98d0df22b2521d762
orig <48911144+origranot@users.noreply.github.com>
2022-10-17 19:46:45 +0300

    change theme to light and added google verification (#244)
    * change theme to light and added google verification
    
    * run prettier
    
    Co-authored-by: orig <oriorigranot@gamil.com>

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,6 @@ module.exports = {
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
-    themes: ['winter', 'dracula'],
+    themes: ['light', 'dracula'],
   },
 };
