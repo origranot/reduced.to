e3c4d3668e982f3e01c9bcd526764a46ca38a5b4
nekiwo <58634064+nekiwo@users.noreply.github.com>
2022-10-17 13:32:11 -0500

    simple rate limiter

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { ShortenerModule } from './shortener/shortener.module';
 import { AppCacheModule } from './cache/cache.module';
 
 @Module({
-  imports: [AppCacheModule, ShortenerModule],
+  imports: [
+    AppCacheModule,
+    ShortenerModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    })
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
