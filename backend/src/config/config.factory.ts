eba979b8456c99fbb4972d992d0281b81d8e1999
orig <oriorigranot@gamil.com>
2022-10-18 23:16:51 +0300

    added config and removed unused packages

---
+++ backend/src/config/config.factory.ts
@@ -0,0 +1,19 @@
+import { ConfigFactory } from '@nestjs/config';
+
+export const configFactory: ConfigFactory<{ config: IConfiguration }> = () => {
+    return {
+        config: {
+            app: {
+                port: +process.env.APP_PORT || 3000,
+            },
+        },
+    };
+};
+
+export interface AppConfig {
+    port: number;
+}
+
+export interface IConfiguration {
+    app: AppConfig;
+}
