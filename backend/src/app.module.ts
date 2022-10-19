ef556d356a686baee4f689dcac4f715f2f148f75
Santosh Bhandari <bsantosh909@gmail.com>
2022-10-19 22:00:28 +0545

    feat: Implement rate limit (#258)
    * feat: Implement rate limit
    
    * perf: Make requested change

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ShortenerModule } from './shortener/shortener.module';
 import { AppCacheModule } from './cache/cache.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+
 import { AppConfigModule } from './config/config.module';
+import { AppConfigService } from './config/config.service';
 
 @Module({
-  imports: [AppConfigModule, AppCacheModule, ShortenerModule],
+  imports: [
+    AppConfigModule,
+    AppCacheModule,
+    ThrottlerModule.forRootAsync({
+      inject: [AppConfigService],
+      useFactory: (config: AppConfigService) => ({
+        ttl: config.getConfig().rateLimit.ttl,
+        limit: config.getConfig().rateLimit.limit,
+      }),
+    }),
+    ShortenerModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
