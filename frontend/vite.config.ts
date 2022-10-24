6db726ae965f9f50a5831da096ede150b4c3a169
Guillem Padilla <guillem.padilla@companial.com>
2022-10-24 03:14:24 +0300

    Env things

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { qwikReact } from '@builder.io/qwik-react';
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {
   return {
