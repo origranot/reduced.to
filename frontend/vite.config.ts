128a76efc24520a8befb6ee22dd6786d980d6327
orig <48911144+origranot@users.noreply.github.com>
2022-10-24 04:48:14 +0300

    load env into vite (#282)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,17 @@
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import { qwikReact } from '@builder.io/qwik-react';
 import { qwikVite } from '@builder.io/qwik/optimizer';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
   return {
     ssr: { target: 'node', format: 'cjs' },
     plugins: [qwikCity(), qwikVite(), tsconfigPaths(), qwikReact()],
+    define: {
+      API_DOMAIN: JSON.stringify(env.API_DOMAIN),
+    },
   };
 });
