e318b398235a333ea640d996e27ca388a04269ac
orig <48911144+origranot@users.noreply.github.com>
2022-10-25 00:54:38 +0300

    fix: docker files (#287)

--- frontend/src/routes/[...urlId]/index.tsx
+++ frontend/src/routes/[...urlId]/index.tsx
@@ -1,23 +1,17 @@
-import { component$, useClientEffect$, useServerMount$, useStore } from '@builder.io/qwik';
+import { component$, useClientEffect$ } from '@builder.io/qwik';
 import { useLocation } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  const store = useStore({
-    urlRedirect: '',
-  });
-
   const location = useLocation();
 
-  useServerMount$(async () => {
+  useClientEffect$(async () => {
+    console.log(process.env.API_DOMAIN);
+
     const urlId = location.params.urlId.replace(/\//g, '');
     const res = await fetch(`${process.env.API_DOMAIN}/api/v1/shortener/${urlId}`);
     const url = await res.text();
 
-    store.urlRedirect = url;
-  });
-
-  useClientEffect$(async () => {
-    window.location.replace(store.urlRedirect.length ? store.urlRedirect : '/unknown');
+    window.location.replace(url || '/unknown');
   });
 
   return <div />;
