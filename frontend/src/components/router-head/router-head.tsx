2ed5b5636daf7c8f1672be49f573d91bfb2dcd6e
Owen Gaspard <owen@ogaspard.com>
2022-10-27 00:00:19 -0500

    Update router-head.tsx

--- frontend/src/components/router-head/router-head.tsx
+++ frontend/src/components/router-head/router-head.tsx
@@ -16,8 +16,6 @@ export const RouterHead = component$(() => {
       <link rel="canonical" href={loc.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="color-scheme" content="dark light" />
-      
-
       <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
@@ -27,9 +25,6 @@ export const RouterHead = component$(() => {
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#fdfdfd" />
-
-
-
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link
