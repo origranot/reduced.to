7724d34038373e601bbf417edc3021e5075548ff
Guillem Padilla <guillem.padilla@companial.com>
2022-10-29 23:47:40 +0300

    Add auth login workflow

---
+++ backend/src/auth/auth.service.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersModule } from '../users/users.module';
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [UsersModule],
+      providers: [AuthService],
+    }).compile();
+
+    service = module.get<AuthService>(AuthService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService, private jwtService: JwtService) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.user({ email: username });
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      firstName: user.name,
+      rule: user.rule,
+      iss: 'reduced.to',
+    };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
