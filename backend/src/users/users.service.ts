ee960280fc506b7d6d9f24c69fa727782bc77bff
Guillem Padilla Martín <guillem.martin.p@gmail.com>
2022-10-29 18:13:45 +0300

    feat users service (#308)
    * Add prisma service
    
    * Add users service
    
    * Add users module and spec

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async user(userWhereUniqueInput: Prisma.UserWhereUniqueInput): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: userWhereUniqueInput,
+    });
+  }
+}
