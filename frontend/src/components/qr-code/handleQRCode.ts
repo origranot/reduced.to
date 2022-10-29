956f7118c61e81b1b932c81244a2726104cfcd2f
ShreyashB <52196231+shreyash-b@users.noreply.github.com>
2022-10-29 14:01:53 +0530

    update naming scheme for qrcode

--- frontend/src/components/qr-code/handleQRCode.ts
+++ frontend/src/components/qr-code/handleQRCode.ts
@@ -5,6 +5,10 @@ export function generateQRCode(url: string, width: number) {
   QRCode.toCanvas(document.querySelector('#qrcode canvas'), url, { width }, (error: any) => {
     if (error) console.error(error);
   });
+  var splitUrl = url.split('/');
+  document
+    .querySelector('#qrcode canvas')
+    ?.setAttribute('data-location', splitUrl[splitUrl.length - 1]);
 }
 
 // Downloads the QR code
@@ -12,6 +16,6 @@ export function downloadQRCode() {
   const canvas = document.querySelector('#qrcode canvas') as HTMLCanvasElement;
   const a = document.createElement('a');
   a.href = canvas.toDataURL('image/png');
-  a.download = 'qrcode.png';
+  a.download = 'qrcode-' + canvas.getAttribute('data-location') + '.png';
   a.click();
 }
