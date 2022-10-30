f6b195e887891dbbecb3370be54547acda54242b
Guillem Padilla Martín <guillem.martin.p@gmail.com>
2022-10-30 20:28:38 +0200

    Add signup endpoint (#313)

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignupDto } from './dto/signup.dto';
 import { LocalAuthGuard } from './guards/local.guard';
 
 @Controller({
@@ -13,5 +14,14 @@ export class AuthController {
   @Post('/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @Post('/signup')
+  async signup(@Body() signupDto: SignupDto) {
+    if (!signupDto.email || !signupDto.password) {
+      throw new BadRequestException();
+    }
+
+    return this.authService.signup(signupDto);
   }
 }
