836f3fa863e66d31f488cb544e87ecdec97c5b24
Guillem Padilla Martín <guillem.martin.p@gmail.com>
2022-10-31 11:09:26 +0200

    Update backend settings (#317)

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,21 +1,8 @@
-import { Controller, Get, Param, Redirect, VERSION_NEUTRAL } from '@nestjs/common';
-import { ShortenerService } from './shortener/shortener.service';
+import { Controller, VERSION_NEUTRAL } from '@nestjs/common';
 
 @Controller({
   version: VERSION_NEUTRAL,
 })
 export class AppController {
-  constructor(private readonly ShortenerService: ShortenerService) {}
-
-  @Get(':path')
-  @Redirect()
-  async redirect(@Param() params: { path: string }) {
-    const originalUrl = await this.ShortenerService.getOriginalUrl(params.path);
-    if (originalUrl !== null) {
-      return { url: originalUrl };
-    }
-
-    // 404, redirect to /unknown
-    return { url: '/unknown' };
-  }
+  constructor() {}
 }
