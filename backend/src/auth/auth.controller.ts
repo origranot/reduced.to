3d7adc3a43bb00cfeb5cb902402545e085db689b
orig <48911144+origranot@users.noreply.github.com>
2022-11-03 21:56:09 +0200

    Implemented basic validation on backend (#326)
    fmt

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 import { SignupDto } from './dto/signup.dto';
 import { LocalAuthGuard } from './guards/local.guard';
 
@@ -12,16 +13,12 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post('/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
   }
 
   @Post('/signup')
   async signup(@Body() signupDto: SignupDto) {
-    if (!signupDto.email || !signupDto.password) {
-      throw new BadRequestException();
-    }
-
     return this.authService.signup(signupDto);
   }
 }
