b29dc032982263f272dcef64e7a0b566ea234e17
Ori Granot <oriorigranot@gmail.com>
2022-11-04 02:01:37 +0200

    fix twitter button

--- frontend/src/components/home-buttons/actions.tsx
+++ frontend/src/components/home-buttons/actions.tsx
@@ -1,21 +1,14 @@
-import { ITwitter } from './types/social-media';
-
 /**
  * function to share on twitter
- * @param twitterData
+ * @param url - the reduced url link
  * @returns
  */
-export function handleShareOnTwitter(twitterData?: ITwitter) {
-  const shorten_url = document.querySelector('#result #text')!.textContent;
-  if (!shorten_url) return;
-  let base_url = `https://twitter.com/share?`;
-  const params: ITwitter = twitterData
-    ? twitterData
-    : {
-        text: `${encodeURIComponent('My url has just reduced!\nCheck out:')}`,
-        url: shorten_url,
-        hashtags: 'reducedto,shortenurl',
-      };
-  base_url += `text=${params.text} &url=${params.url} &hashtags=${params.hashtags} &via=${params.via}`;
-  window.open(base_url, '', 'left=0,top=auto,width=550,height=450');
+export function handleShareOnTwitter(url: string) {
+  if (!url) return;
+
+  const text = `${encodeURIComponent('My url has just reduced!\nCheck out:')}`;
+  const hashtags = 'reducedto,shortenurl';
+
+  const twitterUrl = `https://twitter.com/share?text=${text} &url=${url} &hashtags=${hashtags}`;
+  window.open(twitterUrl, '', 'left=0,top=auto,width=550,height=450');
 }
