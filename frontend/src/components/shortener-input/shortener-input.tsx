201cfb4a63961cfc42cd85daed0c4a480b83733e
jessej-samuel <1234jessej@gmail.com>
2022-11-05 19:52:42 +0530

    ✨ feat: input is focused on page load

--- frontend/src/components/shortener-input/shortener-input.tsx
+++ frontend/src/components/shortener-input/shortener-input.tsx
@@ -24,6 +24,7 @@ export const ShortenerInput = component$((props: ShortenerInputProps) => {
           placeholder="Very long url..."
           aria-label="url"
           aria-describedby="shortenerBtn"
+          autoFocus
         />
         <ShortenerInputBtn disabled={state.inputValue.length === 0} onClick$={props.onSubmit$} />
       </div>
