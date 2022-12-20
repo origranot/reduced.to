af3f96bb632053c404bbdccaf12f67f6090e2ddf
orig <48911144+origranot@users.noreply.github.com>
2022-12-20 20:04:50 +0200

    fix(twitter): Remove encode of the raw text (#348)
    Co-authored-by: orig <oriorigranot@gamil.com>

--- frontend/src/components/home-buttons/actions.tsx
+++ frontend/src/components/home-buttons/actions.tsx
@@ -6,9 +6,9 @@ export function handleShareOnTwitter(url: string) {
   if (!url) return;
 
   const params = new URLSearchParams();
-  params.set('text', encodeURIComponent('My URL has just been reduced! 🎉'));
+  params.set('text', 'My URL has just been reduced! 🎉\nCheck it out:');
   params.set('url', url);
-  params.set('hashtags', 'reduced.to,shortenurl');
+  params.set('hashtags', 'reducedto,shortenurl');
 
   const twitterUrl = `https://twitter.com/share?${params.toString()}`;
   window.open(twitterUrl, '', 'left=0,top=auto,width=550,height=450');
