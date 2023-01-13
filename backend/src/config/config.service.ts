279b1be68ced0803f107df54c70211e23be2b44b
orig <48911144+origranot@users.noreply.github.com>
2023-01-13 22:12:50 +0200

    tests(app): added multiple unit tests cases (#360)
    tests(config): added tests to config service

--- backend/src/config/config.service.ts
+++ backend/src/config/config.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { IConfiguration } from './config.factory';
+import { Configuration } from './config.factory';
 
 @Injectable()
 export class AppConfigService {
   constructor(private configService: ConfigService) {}
 
-  getConfig(): IConfiguration {
-    return this.configService.get('config', { infer: true });
+  getConfig(): Configuration {
+    const config = this.configService.get<Configuration>('config');
+    if (!config) {
+      throw new ConfigNotFoundError();
+    }
+    return config;
+  }
+}
+
+export class ConfigNotFoundError extends Error {
+  constructor() {
+    super('Configuration not found');
   }
 }
