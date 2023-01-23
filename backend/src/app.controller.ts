a6d96a5dffc20ffa1bba76d53d4edda126f7450a
orig <oriorigranot@gmail.com>
2023-01-23 17:30:01 +0200

    chore(app): Removed app controller

--- backend/src/app.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, VERSION_NEUTRAL } from '@nestjs/common';
-
-@Controller({
-  version: VERSION_NEUTRAL,
-})
-export class AppController {
-  constructor() {}
-}
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { PrismaService } from './prisma/prisma.service';
 import { Module } from '@nestjs/common';
 import { ThrottlerModule } from '@nestjs/throttler';
-import { AppController } from './app.controller';
 import { AppCacheModule } from './cache/cache.module';
 import { ShortenerModule } from './shortener/shortener.module';
-
 import { AuthModule } from './auth/auth.module';
 import { AppConfigModule } from './config/config.module';
 import { AppConfigService } from './config/config.service';
@@ -26,7 +24,6 @@ import { NovuModule } from './novu/novu.module';
     AuthModule,
     NovuModule,
   ],
-  controllers: [AppController],
   providers: [PrismaService, UniqueConstraint],
 })
 export class AppModule {}
