0d215460e8c1ed50131636da6a5601e4f9913e6d
orig <48911144+origranot@users.noreply.github.com>
2023-01-31 21:21:45 +0200

    Qwik migrate v0.16.0 (#369)
    * deps(qwik): migrate to latest qwik release
    
    * fix(front): uses LexicalScope
    
    * fix(qwik): syntax and deps

--- frontend/src/entry.ssr.tsx
+++ frontend/src/entry.ssr.tsx
@@ -1,5 +1,16 @@
-import { renderToStream } from '@builder.io/qwik-react';
-import type { RenderToStreamOptions } from '@builder.io/qwik/server';
+/**
+ * WHAT IS THIS FILE?
+ *
+ * SSR entry point, in all cases the application is render outside the browser, this
+ * entry point will be the common one.
+ *
+ * - Server (express, cloudflare...)
+ * - npm run start
+ * - npm run preview
+ * - npm run build
+ *
+ */
+import { renderToStream, RenderToStreamOptions } from '@builder.io/qwik/server';
 import { manifest } from '@qwik-client-manifest';
 import Root from './root';
 
@@ -7,8 +18,10 @@ export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
     manifest,
     ...opts,
+    // Use container attributes to set attributes on the html tag.
     containerAttributes: {
-      lang: 'en',
+      lang: 'en-us',
+      ...opts.containerAttributes,
     },
   });
 }
