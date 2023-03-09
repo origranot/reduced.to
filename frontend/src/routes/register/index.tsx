5fbc91f7310e4e0a79103832a9c0a26adfc2f853
orig <oriorigranot@gmail.com>
2023-03-09 20:03:17 +0200

    fmt

--- frontend/src/routes/register/index.tsx
+++ frontend/src/routes/register/index.tsx
@@ -18,7 +18,8 @@ export const onGet: RequestHandler = async ({ cookie, redirect }) => {
 };
 
 export const useRegister = globalAction$(
-  async ({ displayName, email, password }, { fail, headers, cookie }) => {
+  () => {
+    // async ({ displayName, email, password }, { fail, headers, cookie }) => {
     // const data = await fetch(`${process.env.API_DOMAIN}/api/v1/auth/signup`, {
     //   method: 'POST',
     //   headers: {
