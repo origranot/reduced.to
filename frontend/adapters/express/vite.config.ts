e98f7413ce39ed291c1218bf1d9fcdb6d8b15b18
orig <oriorigranot@gmail.com>
2023-03-10 22:16:55 +0200

    fix(adapter): use express adapter

---
+++ frontend/adapters/express/vite.config.ts
@@ -0,0 +1,22 @@
+import { expressAdapter } from '@builder.io/qwik-city/adapters/express/vite';
+import { extendConfig } from '@builder.io/qwik-city/vite';
+import baseConfig from '../../vite.config';
+
+export default extendConfig(baseConfig, () => {
+  return {
+    build: {
+      ssr: true,
+      rollupOptions: {
+        input: ['src/entry.express.tsx', '@qwik-city-plan'],
+      },
+    },
+    plugins: [
+      expressAdapter({
+        ssg: {
+          origin: `httpps://${process.env.DOMAIN}`,
+          include: [],
+        },
+      }),
+    ],
+  };
+});
