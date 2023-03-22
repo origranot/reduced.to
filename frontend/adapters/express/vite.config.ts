fb7fd268bc6e10352788caec9e7e4949826bc1be
orig <oriorigraont@gmail.com>
2023-03-22 22:28:51 +0200

    fix(typo): fix vite config typo

--- frontend/adapters/express/vite.config.ts
+++ frontend/adapters/express/vite.config.ts
@@ -13,7 +13,7 @@ export default extendConfig(baseConfig, () => {
     plugins: [
       expressAdapter({
         ssg: {
-          origin: `httpps://${process.env.DOMAIN}`,
+          origin: `https://${process.env.DOMAIN}`,
           include: [],
         },
       }),
