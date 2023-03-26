dfa16f112192ac0963efb2ae50d89485fee9e66d
orig <oriorigranot@gmail.com>
2023-03-26 20:44:28 +0300

    fix(logout)

--- frontend/src/routes/logout/index.ts
+++
@@ -1,7 +0,0 @@
-import { RequestHandler } from '@builder.io/qwik-city';
-
-export const onGet: RequestHandler = async ({ cookie, redirect }) => {
-  cookie.delete('accessToken', { path: '/' });
-  cookie.delete('refreshToken', { path: '/' });
-  throw redirect(302, '/');
-};
