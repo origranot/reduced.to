1ec70ed7355ecef51dec029dd44a0af453adab08
orig <oriorigranot@gmail.com>
2023-03-26 21:05:53 +0300

    fix(redirect)

--- frontend/src/routes/logout/index.ts
+++ frontend/src/routes/logout/index.ts
@@ -1,7 +1,8 @@
 import { RequestHandler } from '@builder.io/qwik-city';
+import { ACCESS_COOKIE_NAME, REFRESH_COOKIE_NAME } from '../../shared/auth.service';
 
 export const onGet: RequestHandler = async ({ cookie, redirect }) => {
-  cookie.delete('accessToken', { path: '/' });
-  cookie.delete('refreshToken', { path: '/' });
-  redirect(302, '/');
+  cookie.delete(ACCESS_COOKIE_NAME, { path: '/' });
+  cookie.delete(REFRESH_COOKIE_NAME, { path: '/' });
+  throw redirect(302, '/');
 };
