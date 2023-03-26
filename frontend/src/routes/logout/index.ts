dca45378f616d27a7ab4957044a98fcfbe55d18f
orig <oriorigranot@gmail.com>
2023-03-26 21:27:20 +0300

    fix(cookie): add domain to logout route

--- frontend/src/routes/logout/index.ts
+++ frontend/src/routes/logout/index.ts
@@ -2,7 +2,8 @@ import { RequestHandler } from '@builder.io/qwik-city';
 import { ACCESS_COOKIE_NAME, REFRESH_COOKIE_NAME } from '../../shared/auth.service';
 
 export const onGet: RequestHandler = async ({ cookie, redirect }) => {
-  cookie.delete(ACCESS_COOKIE_NAME, { path: '/' });
-  cookie.delete(REFRESH_COOKIE_NAME, { path: '/' });
+  const domain = process.env.NODE_ENV === 'production' ? `.${process.env.DOMAIN}` : 'localhost';
+  cookie.delete(ACCESS_COOKIE_NAME, { path: '/', domain });
+  cookie.delete(REFRESH_COOKIE_NAME, { path: '/', domain });
   throw redirect(302, '/');
 };
