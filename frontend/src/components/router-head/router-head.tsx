c7999d34fdccf67736b91760a23d1f5fa6904482
orig <48911144+origranot@users.noreply.github.com>
2023-04-29 19:17:17 +0300

    chore(frontend): update devDependencies and dependencies (#379)
    refactor(router-head.tsx): remove unused useLocation hook and link rel="canonical" tag

--- frontend/src/components/router-head/router-head.tsx
+++ frontend/src/components/router-head/router-head.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik';
-import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
+import { useDocumentHead } from '@builder.io/qwik-city';
 import { ThemeScript } from './theme-script';
 
 /**
@@ -7,13 +7,11 @@ import { ThemeScript } from './theme-script';
  */
 export const RouterHead = component$(() => {
   const head = useDocumentHead();
-  const loc = useLocation();
 
   return (
     <>
       <title>{head.title}</title>
 
-      <link rel="canonical" href={loc.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="color-scheme" content="dark light" />
       <link rel="icon" type="image/png" href="/favicon.png" />
