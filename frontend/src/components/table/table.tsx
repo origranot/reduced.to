90b06d7886d880bbdbd1c5d9444d466487713042
darkmift <darkmift@gmail.com>
2023-05-24 23:33:13 +0300

    [user-table] added first component draft

---
+++ frontend/src/components/table/table.tsx
@@ -0,0 +1,111 @@
+import { component$, useStylesScoped$, useStore } from '@builder.io/qwik';
+import styles from './table.css?inline';
+import {
+  createTable,
+  createColumnHelper,
+  getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
+  ColumnDef,
+} from '@tanstack/table-core';
+
+type RowType = { [key: string]: unknown };
+
+export interface TableProps {
+  rows: RowType[];
+  customColumnNames?: { [key in keyof RowType]?: string };
+}
+
+export function generateUseTable(columns: ColumnDef<RowType, unknown>[], rows: RowType[]) {
+  const defaultTable = createTable({
+    columns,
+    data: [],
+    state: {},
+    renderFallbackValue: null,
+    onStateChange: () => {},
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  const useTable = (tableState: { sorting: SortingState }) =>
+    createTable({
+      columns,
+      data: rows,
+      state: { ...defaultTable.initialState, sorting: tableState.sorting },
+      renderFallbackValue: 'fallback',
+      onStateChange: (newState) => {
+        if (typeof newState === 'function') {
+          // TODO: newState might be a value rather than a function.
+          tableState.sorting = newState(tableState as any /*tanstack ts issue*/).sorting;
+        }
+      },
+      getSortedRowModel: getSortedRowModel(),
+      getCoreRowModel: getCoreRowModel(),
+    });
+
+  return useTable;
+}
+
+export const DataTable = component$((props: TableProps) => {
+  useStylesScoped$(styles);
+
+  const columnHelper = createColumnHelper<RowType>();
+  const columns = Object.keys(props.rows[0]).map((colName: string) =>
+    columnHelper.accessor(colName, {
+      header: props.customColumnNames?.[colName] || colName,
+    })
+  );
+
+  const state = useStore<{ sorting: SortingState }>({
+    sorting: [],
+  });
+
+  // const table = useTable(state);
+  const table = generateUseTable(columns as ColumnDef<RowType, unknown>[], props.rows)(state);
+
+  return (
+    <div class="overflow-x-auto">
+      <table id="table" class="table table-zebra w-full">
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(({ column }) => {
+                const id = column.id;
+                return (
+                  <th
+                    key={id}
+                    onClick$={(e) => {
+                      console.log('🚀 ~ file: table.tsx:94 ~ {headerGroup.headers.map ~ e:', {
+                        e,
+                        id,
+                      });
+
+                      const table = generateUseTable(
+                        columns as ColumnDef<RowType, unknown>[],
+                        props.rows
+                      )(state);
+
+                      table.getColumn(id)?.getToggleSortingHandler?.()?.(e);
+                    }}
+                  >
+                    {column.columnDef.header}
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getAllCells().map((cell) => (
+                <td key={cell.id}>{cell.getValue<string>()}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+        <tfoot></tfoot>
+      </table>
+    </div>
+  );
+});
