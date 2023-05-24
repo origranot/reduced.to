55563a6c3871f37b5fd325908b6c5db087b3522a
darkmift <darkmift@gmail.com>
2023-05-24 23:33:31 +0300

    [user-table] use of table in dashboard

--- frontend/src/routes/dashboard/admin/index.tsx
+++ frontend/src/routes/dashboard/admin/index.tsx
@@ -1,11 +1,34 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import { DataTable } from '~/components/table/table';
+
+export const userMockData = [
+  {
+    id: '1abadc9c-2e8a-4c40-aa43-6f5f3af6f63d',
+    name: 'John Smith',
+    email: 'john@j.com',
+    role: 'ADMIN',
+    verified: true,
+    verificationToken: 'eyJhbGciOiJIUzI1NiIs',
+    refreshToken: '$2b$10$/VwWgAiRYqF6',
+  },
+  {
+    id: '1abadc9c-2e8a-4c40-aa43-6f5f3af6f63d',
+    name: 'Jane Doe',
+    email: 'jane@j.com',
+    role: 'USER',
+    verified: true,
+    verificationToken: 'eyJhbGciOiJIUzI1NiIs',
+    refreshToken: '$2b$10$/VwWgAiRYqF6',
+  },
+];
 
 export default component$(() => {
   return (
     <>
       <h1>Admin panel</h1>
       <p class="text-red-700">//TODO: Implement a table of users</p>
+      <DataTable rows={userMockData} customColumnNames={{ name: 'User-Name' }} />
     </>
   );
 });
