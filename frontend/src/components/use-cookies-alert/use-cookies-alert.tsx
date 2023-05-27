9553ee0a493fad0bb464a2a327ff4c4f7d4e51e7
orig <48911144+origranot@users.noreply.github.com>
2023-05-27 16:24:35 +0300

    Add "We use cookies" alert, including a privacy policy. (#399)
    * style(license): update year and add parentheses to license name
    feat(use-cookies-alert.tsx): add UseCookiesAlert component
    feat(layout.tsx): add UseCookiesAlert component to layout
    feat(privacy-policy): add privacy policy page and document head metadata
    
    * refactor(use-cookies-alert.tsx): change visible variable declaration from let to const

---
+++ frontend/src/components/use-cookies-alert/use-cookies-alert.tsx
@@ -0,0 +1,45 @@
+import { component$, $, useSignal } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
+
+export interface UseCookiesAlertProps {
+  visible: boolean;
+}
+
+export const ACCEPT_COOKIES_COOKIE_NAME = 'accept-cookies';
+
+export const UseCookiesAlert = component$((props: UseCookiesAlertProps) => {
+  const visible = useSignal<boolean>(props.visible);
+  const acceptCookies = $(() => {
+    document.cookie = `${ACCEPT_COOKIES_COOKIE_NAME}=true; max-age=31536000; path=/`;
+    visible.value = false;
+  });
+  return (
+    <>
+      {visible.value ? (
+        <>
+          <div class="fixed flex flex-col bottom-4 right-4 rounded-lg bg-base-100 shadow-md p-6">
+            <span class="text-md mb-3 max-w-[17rem]">
+              We use cookies to automatically save and load your preferences.
+            </span>
+            <div class="flex items-center gap-2">
+              <Link
+                href="/privacy-policy"
+                class="btn btn-ghost flex-1 text-xs whitespace-nowrap mr-5"
+              >
+                Privacy Policy
+              </Link>
+              <button
+                onClick$={acceptCookies}
+                class="btn btn-primary relative flex items-center gap-3 transition ease-in-out border px-4 py-2 rounded-md"
+              >
+                Okay
+              </button>
+            </div>
+          </div>
+        </>
+      ) : (
+        ''
+      )}
+    </>
+  );
+});
