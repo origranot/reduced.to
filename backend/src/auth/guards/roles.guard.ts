626569682d813d1fda6a905cb445d02287b2ea40
orig <oriorigranot@gmail.com>
2023-05-28 22:17:58 +0300

    fix(roles.guard.spec.ts): throw UnauthorizedException instead of returning false when user does not have required role fix(roles.guard.ts): throw UnauthorizedException instead of returning false when user does not have required role feat(users.controller.spec.ts): override RolesGuard to always return true in tests to avoid role-based authorization issues

--- backend/src/auth/guards/roles.guard.ts
+++ backend/src/auth/guards/roles.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from '@prisma/client';
 import { ROLES_KEY } from '../../shared/decorators/roles/roles.decorator';
@@ -17,7 +17,7 @@ export class RolesGuard implements CanActivate {
     const user = request.user as UserContext;
 
     if (!user || !matchRoles(roles, user.role)) {
-      return false;
+      throw new UnauthorizedException();
     }
 
     return true;
