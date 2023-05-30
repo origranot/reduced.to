2951fc72d4ed84d097a9f33058bd9177c8ff42a0
achiyahb <60642470+achiyahb@users.noreply.github.com>
2023-05-30 21:58:43 +0300

    Custom expiration time (#391)
    * create user's short url with optional expiration time
    
    * use one endpoint for users and guest
    
    * fix tests
    
    * add units tests
    
    * extend JwtAuthGuard
    
    * naming short -> shortened
    
    * fix tests
    
    * move logic from controller to service
    
    * fix typo
    
    * add credential cookies on Post: shortener endpoint
    
    * fix expiration time condition
    
    * fix tests
    
    * convert expiration time to ttl and set it
    
    * small naming fixes
    
    * use redis config instead of new cache config
    
    * use redis config instead of new cache config
    
    * create ttl util provider
    
    * temp fix tests
    
    * remove premium url from spec
    
    * some specs test fixes (typos and describes wrappers)
    
    * fix shortener controller tests
    
    * add tests for new shortener service new methods
    
    * tests for ttl util
    
    * moved ttl util to right place and use export const instead of injectable class
    
    * change expiration type to Date
    
    * add constants mock to service specs files
    
    * start with should
    
    * use constant instead of strings
    
    * naming short -> shortener
    
    * fix tests issue
    
    ---------
    
    Co-authored-by: orig <48911144+origranot@users.noreply.github.com>

--- backend/src/shortener/shortener.service.ts
+++ backend/src/shortener/shortener.service.ts
@@ -1,27 +1,48 @@
-import { AppCacheService } from './../cache/cache.service';
-import { Injectable } from '@nestjs/common';
+import { AppCacheService } from '../cache/cache.service';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AppConfigService } from '../config/config.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { ShortenerDto } from './dto';
+import { UserContext } from '../auth/interfaces/user-context';
+import { convertExpirationTimeToTtl } from '../cache/utils/ttl.util';
 
 @Injectable()
 export class ShortenerService {
-  constructor(private readonly appCacheService: AppCacheService, private readonly appConfigService: AppConfigService) {}
+  constructor(
+    private readonly appCacheService: AppCacheService,
+    private readonly prisma: PrismaService,
+    private readonly appConfigService: AppConfigService
+  ) {}
 
   /**
-   * Return the original url of the specific url.
-   * @param {String} shortUrl The short url
-   * @returns {String} Returns the original url or null
+   * Retrieves the original URL associated with a given shortened URL.
+   * @param {string} shortenedUrl - The shortened URL.
+   * @returns {Promise<string|null>} - The original URL if found, otherwise null.
    */
-  getOriginalUrl = async (shortUrl: string): Promise<string> => {
-    const originalUrl = await this.appCacheService.get(shortUrl);
+  getOriginalUrl = async (shortenedUrl: string): Promise<string | null> => {
+    const originalUrl = await this.getUrlFromCache(shortenedUrl);
+    if (originalUrl) {
+      return originalUrl;
+    }
+    return this.getUrlFromDb(shortenedUrl);
+  };
+
+  /**
+   * Retrieves the original URL from the cache based on a given shortened URL.
+   * @param {string} shortenedUrl - The shortened URL.
+   * @returns {Promise<string|null>} - The original URL if found in the cache, otherwise null.
+   */
+  getUrlFromCache = async (shortenedUrl: string): Promise<string> => {
+    const originalUrl = await this.appCacheService.get(shortenedUrl);
     return originalUrl ? originalUrl.toString() : null;
   };
 
   /**
-   * Checks if the url has already been shortend by a using a Regular Expression.
+   * Checks if the url has already been shortened by a using a Regular Expression.
    * @param {String} shortUrl The short url
    * @returns {Boolean} Returns a boolean
    */
-  isUrlAlreadyShortend = (shortUrl: string): boolean => {
+  isUrlAlreadyShortened = (shortUrl: string): boolean => {
     const domainRegex = new RegExp(this.appConfigService.getConfig().front.domain);
     return domainRegex.test(shortUrl);
   };
@@ -30,30 +51,119 @@ export class ShortenerService {
    * Generating the short url
    * @returns {String} Returns a random 5 characters url
    */
-  generateShortUrl = (): string => {
+  generateShortenedUrl = (): string => {
     return Math.random().toString(36).substring(2, 7);
   };
 
   /**
-   * Check if the short url is not in use.
-   * @param {String} shortUrl The shorten url.
-   * @returns {Boolean} Is the shortener url taken
+   * Checks if a shortened URL is available (not in use).
+   * @param {string} shortenedUrl - The shortened URL.
+   * @returns {Promise<boolean>} - True if the shortened URL is available, false otherwise.
    */
-  isShortUrlAvailable = async (shortUrl: string): Promise<boolean> => {
-    const originalUrl = await this.getOriginalUrl(shortUrl);
+  isShortenedUrlAvailable = async (shortenedUrl: string): Promise<boolean> => {
+    const originalUrl = await this.getOriginalUrl(shortenedUrl);
     return originalUrl === null;
   };
 
   /**
    * Add the short url to the server routes.
    * @param {String} originalUrl The original url.
-   * @param {String} shortUrl The shorten url.
+   * @param {String} shortenedUrl The shorten url.
+   * @param {String} expirationTime The expiration time.
    */
-  addUrl = async (originalUrl: string, shortUrl: string) => {
-    const isShortUrlAvailable = await this.isShortUrlAvailable(shortUrl);
-    if (!isShortUrlAvailable) {
-      throw new Error('Short URL already taken');
+  addUrl = async (originalUrl: string, shortenedUrl: string, expirationTime?: Date) => {
+    const isShortenedUrlAvailable = await this.isShortenedUrlAvailable(shortenedUrl);
+    if (!isShortenedUrlAvailable) {
+      throw new Error('Shortened URL already taken');
     }
-    await this.appCacheService.set(shortUrl, originalUrl);
+    const ttl = convertExpirationTimeToTtl(expirationTime);
+    if (!ttl) {
+      await this.appCacheService.set(shortenedUrl, originalUrl);
+    } else {
+      const smallerTtl = Math.min(ttl, this.appConfigService.getConfig().redis.ttl);
+      await this.appCacheService.set(shortenedUrl, originalUrl, smallerTtl);
+    }
   };
+
+  /**
+   * Create a short URL based on the provided data.
+   * @param {string} originalUrl - The original URL.
+   * @param {string} [expirationTime] - The expiration time.
+   * @returns {Promise<{ newUrl: string }>} Returns an object containing the newly created short URL.
+   */
+  createShortenedUrl = async (
+    originalUrl: string,
+    expirationTime?: Date
+  ): Promise<{
+    newUrl: string;
+  }> => {
+    let parsedUrl: URL;
+    try {
+      parsedUrl = new URL(originalUrl);
+
+      // Checks if the URL is already reduced.
+      if (this.isUrlAlreadyShortened(originalUrl)) {
+        throw new Error('The URL is already shortened...');
+      }
+    } catch (err: any) {
+      throw new BadRequestException(err.message || 'URL is invalid');
+    }
+
+    let shortUrl: string;
+
+    do {
+      shortUrl = this.generateShortenedUrl();
+    } while (!(await this.isShortenedUrlAvailable(shortUrl)));
+
+    await this.addUrl(parsedUrl.href, shortUrl, expirationTime);
+    return { newUrl: shortUrl };
+  };
+
+  /**
+   * Create a db URL based on the provided data and user context.
+   * @param {ShortenerDto} body The data for creating a db URL.
+   * @param {UserContext} user The user context.
+   * @param {string} newUrl The new short URL.
+   * @returns {Promise<any>} Returns the created db URL.
+   */
+  async createDbUrl(body: ShortenerDto, user: UserContext, newUrl: string) {
+    return await this.prisma.url.create({
+      data: {
+        shortenedUrl: newUrl,
+        originalUrl: body.originalUrl,
+        userId: user.id,
+        description: body.description,
+        expirationTime: body.expirationTime,
+      },
+    });
+  }
+
+  /**
+   * Retrieves the original URL associated with a given short URL from the database.
+   * @param {string} shortenedUrl - The short URL.
+   * @returns {Promise<string|null>} - The original URL if the original URL is found and valid, otherwise null.
+   */
+  async getUrlFromDb(shortenedUrl: string) {
+    const url = await this.prisma.url.findFirst({
+      where: {
+        shortenedUrl,
+      },
+    });
+    if (url?.expirationTime && url.expirationTime < new Date()) {
+      return null;
+    }
+    return url ? url.originalUrl : null;
+  }
+
+  /**
+   * Creates a shortened URL for a user based on the provided data.
+   * @param {UserContext} user - The user context.
+   * @param {ShortenerDto} body - The request body containing the original URL and optional expiration time.
+   * @returns {Promise<{ newUrl: string }>} - Returns an object containing the newly created short URL.
+   */
+  async createUsersShortenedUrl(user: UserContext, body: ShortenerDto) {
+    const { newUrl } = await this.createShortenedUrl(body.originalUrl, new Date(body.expirationTime));
+    await this.createDbUrl(body, user, newUrl);
+    return { newUrl };
+  }
 }
--- frontend/src/components/shortener-input/handleShortener.ts
+++ frontend/src/components/shortener-input/handleShortener.ts
@@ -9,6 +9,7 @@ import { copyToClipboard, normalizeUrl } from '~/utils';
 const getShortenUrl = async (originalUrl: string) => {
   const result = await fetch(`${process.env.API_DOMAIN}/api/v1/shortener`, {
     method: 'POST',
+    credentials: 'include',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ originalUrl }),
   });
