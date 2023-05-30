1101bc78ff351284b4caedd0d7a951d44cdfa785
orig <oriorigranot@gmail.com>
2023-05-30 22:04:58 +0300

    refactor(optional-jwt-auth.guard.ts): add types to handleRequest parameters to improve code readability and maintainability

--- backend/src/auth/guards/optional-jwt-auth.guard.ts
+++ backend/src/auth/guards/optional-jwt-auth.guard.ts
@@ -2,7 +2,7 @@ import { JwtAuthGuard } from './jwt.guard';
 
 export class OptionalJwtAuthGuard extends JwtAuthGuard {
   // Overrides the 'handleRequest' method to prevent throwing an error and always returns the user.
-  handleRequest(err, user) {
+  handleRequest(err: any, user: any) {
     return user;
   }
 }
