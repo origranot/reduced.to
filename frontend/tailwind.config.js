ed5669b29b86c5ce602fa18e3b69c5c055e8bb4a
dlibis <thefonz122@gmail.com>
2023-06-02 23:51:23 +0300

    added animation config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fade: {
+          '0%': { opacity: 0, transform: 'translateY(-50px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fade: 'fade 1s ease-in 0s 1 normal forwards',
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
