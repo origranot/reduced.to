2ae6fee6175a6e4022aea84abdaa27453b240775
dlibis <thefonz122@gmail.com>
2023-06-03 10:50:01 +0300

    added helper text

--- frontend/src/components/shortener-input/shortener-input.tsx
+++ frontend/src/components/shortener-input/shortener-input.tsx
@@ -5,7 +5,6 @@ import {
   useOnDocument,
   useSignal,
   QwikKeyboardEvent,
-  QwikChangeEvent,
 } from '@builder.io/qwik';
 import { InputContext } from '~/routes';
 import { ShortenerInputBtn } from './shortener-input-btn';
@@ -24,8 +23,6 @@ export const ShortenerInput = component$((props: ShortenerInputProps) => {
   const searchInput = useSignal<HTMLInputElement>();
   const selectInputValue = useSignal<string>(timeFrameArr[0].key);
 
-  // TODO: add role functionality
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const role = useGetCurrentUser().value?.role;
 
   const handleSelectExpiredTime = $((value: any, key: any) => {
@@ -45,7 +42,13 @@ export const ShortenerInput = component$((props: ShortenerInputProps) => {
 
   return (
     <div class="form-control">
-      <div class="sm:input-group mb-3 flex-col sm:flex-row gap-2 sm:gap-0 ">
+      <div class="self-end  mr-[55px] hidden md:block">
+        <p class="ml-[100px] text-2xl absolute " style={'font-family:Shadows Into Light Two'}>
+          Expiration Time
+        </p>
+        <img src="/images/arrrow-doodle.svg" class="max-w-[100px] rotate-[25deg]" />
+      </div>
+      <div class="md:input-group flex items-stretch mb-3 flex-col md:flex-row gap-2 md:gap-0 ">
         <input
           ref={searchInput}
           onKeyUp$={props.onKeyUp$}
@@ -53,38 +56,42 @@ export const ShortenerInput = component$((props: ShortenerInputProps) => {
           value={state.inputValue}
           type="text"
           id="urlInput"
-          class="input input-bordered focus:outline-0 bg-base-200 flex-auto w-full sm:w-auto mb-2 self-end"
+          class="input input-bordered focus:outline-0 bg-base-200 flex-auto w-full md:w-auto mb-2 self-end"
           placeholder="Very long url..."
           aria-label="url"
           aria-describedby="shortenerBtn"
         />
         <div>
           <Select disabled={role ? false : true} selectInputValue={selectInputValue}>
-            {timeFrameArr.map(({ key, value }) => (
-              <li
-                value={value}
-                key={key}
-                class="w-auto cursor-pointer pl-4 py-1 hover:bg-gray-200"
-                onClick$={$(() => handleSelectExpiredTime(value, key))}
-              >
-                {key}
-              </li>
-            ))}
-
-            {role === 'ADMIN' && (
-              <>
-                <hr />
+            <>
+              {timeFrameArr.map(({ key, value }) => (
                 <li
-                  key={'never'}
+                  value={value}
+                  key={key}
                   class="w-auto cursor-pointer pl-4 py-1 hover:bg-gray-200"
-                  onClick$={$(() => handleSelectExpiredTime(null, 'Never'))}
+                  onClick$={$(() => handleSelectExpiredTime(value, key))}
                 >
-                  Never
+                  {key}
                 </li>
-              </>
-            )}
+              ))}
+
+              {role === 'ADMIN' && (
+                <>
+                  <hr />
+                  <li
+                    key={'never'}
+                    class="w-auto cursor-pointer pl-4 py-1 hover:bg-gray-200"
+                    onClick$={$(() => handleSelectExpiredTime(null, 'Never'))}
+                  >
+                    Never
+                  </li>
+                </>
+              )}
+            </>
           </Select>
+          <div class="text-xs text-gray-500 text-start px-2 pt-1 md:none">Expiration Time</div>
         </div>
+
         <ShortenerInputBtn disabled={state.inputValue.length === 0} onClick$={props.onSubmit$} />
       </div>
     </div>
