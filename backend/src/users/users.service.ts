3cfafb8043441b998fed3ea53e64ca9d8945e50c
orig <oriorigranot@gmail.com>
2023-06-04 22:43:15 +0300

    feat(users): add pagination support to findAll method in UsersController and UsersService
    - Add IPaginationResult interface to shared/utils/pagination/pagination.ts
    - Modify findAll method in UsersService to return IPaginationResult
    - Modify findAll method in UsersController to return IPaginationResult
    - Modify users.controller.spec.ts to test pagination functionality
    
    fix(ttl.spec.ts, shortener.service.spec.ts, users.service.spec.ts): use jest.useFakeTimers() and jest.useRealTimers() to avoid side effects in other tests

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,30 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
+import { IPaginationResult } from '../shared/utils';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  findAll = async (options: IFindAllOptions): Promise<Partial<User>[]> => {
+  findAll = async (options: IFindAllOptions): Promise<IPaginationResult<User>> => {
     const { skip, limit, filter } = options;
 
-    return this.prismaService.user.findMany({
-      select: {
-        id: true,
-        name: true,
-        email: true,
-        role: true,
-        verified: true,
-      },
-      ...(skip && { skip }),
-      take: limit,
-      ...(filter && {
-        where: {
-          OR: [{ email: { contains: filter } }, { name: { contains: filter } }],
+    const result = await this.prismaService.$transaction([
+      this.prismaService.user.count({}),
+      this.prismaService.user.findMany({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          role: true,
+          verified: true,
         },
+        ...(skip && { skip }),
+        take: limit,
+        ...(filter && {
+          where: {
+            OR: [{ email: { contains: filter } }, { name: { contains: filter } }],
+          },
+        }),
       }),
-    });
+    ]);
+
+    return {
+      total: result[0],
+      data: result[1],
+    };
   };
 }
 
