668650c7216dd9b076a24d2c15f1edbf0f91832b
dlibis <thefonz122@gmail.com>
2023-06-05 22:47:31 +0300

    added never to time frame dir

--- frontend/src/components/shortener-input/constants.ts
+++ frontend/src/components/shortener-input/constants.ts
@@ -1,7 +1,8 @@
 export const DAY_IN_MILLISECONDS = 1000 * 24 * 60 * 60;
 
 export const TIME_FRAME_DIR = {
-  DAY: { key: 'DAY', value: DAY_IN_MILLISECONDS },
-  THREE_DAYS: { key: '3 Days', value: DAY_IN_MILLISECONDS * 3 },
-  ONE_WEEK: { key: '1 Week', value: DAY_IN_MILLISECONDS * 7 },
+  DAY: { name: 'Day', value: DAY_IN_MILLISECONDS },
+  THREE_DAYS: { name: '3 Days', value: DAY_IN_MILLISECONDS * 3 },
+  ONE_WEEK: { name: '1 Week', value: DAY_IN_MILLISECONDS * 7 },
+  NEVER: { name: '1 Week', value: null },
 };
