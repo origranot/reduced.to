ce70a3ea2986535f39241fa09f0e0965a8b07949
dlibis <thefonz122@gmail.com>
2023-06-05 21:44:09 +0300

    move const file

--- frontend/src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const DAY_IN_MILLISECONDS = 1000 * 24 * 60 * 60;
-
-export const timeFrameArr = [
-  { key: 'Day', value: DAY_IN_MILLISECONDS },
-  { key: '3 Days', value: DAY_IN_MILLISECONDS * 3 },
-  { key: '1 Week', value: DAY_IN_MILLISECONDS * 7 },
-];
