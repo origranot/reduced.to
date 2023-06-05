8897a5fc8247a59ce2ad41206b261025f8d7ac0f
dlibis <63005208+dlibis@users.noreply.github.com>
2023-06-05 23:31:56 +0300

    394 add expired time (#400)
    * double one week name
    
    * remove class
    
    * hover colors
    
    ---------
    
    Co-authored-by: orig <48911144+origranot@users.noreply.github.com>

--- frontend/src/components/shortener-input/constants.ts
+++ frontend/src/components/shortener-input/constants.ts
@@ -4,5 +4,5 @@ export const TIME_FRAME_DIR = {
   DAY: { name: 'Day', value: DAY_IN_MILLISECONDS },
   THREE_DAYS: { name: '3 Days', value: DAY_IN_MILLISECONDS * 3 },
   ONE_WEEK: { name: '1 Week', value: DAY_IN_MILLISECONDS * 7 },
-  NEVER: { name: '1 Week', value: null },
+  NEVER: { name: 'Never', value: null },
 };
