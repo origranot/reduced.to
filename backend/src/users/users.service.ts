8f9110f2b0c715900fc0be9fae20e89a2ddac0d2
achiyahb <60642470+achiyahb@users.noreply.github.com>
2023-06-07 21:37:09 +0300

    Sort user fields (#410)
    * sort user fields
    
    * set whitelist locally
    
    * fix tests
    
    * use uppercase for enum keys
    
    * tests sort cases and whitelist
    
    * orderByBuilder tests
    
    * naming -> ORDER_BY_CLAUSE
    
    * test sort inside objectContaining
    
    * set whitelist globally
    
    * use 'sort' as a constant
    
    * remove test on whitelist

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
-import { IPaginationResult } from '../shared/utils';
+import { IPaginationResult, orderByBuilder } from '../shared/utils';
+import { SortUserDto } from './dto/sort-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
   findAll = async (options: IFindAllOptions): Promise<IPaginationResult<User>> => {
-    const { skip, limit, filter } = options;
+    const { skip, limit, filter, sort } = options;
 
     const WHERE_CLAUSE = {
       OR: [{ email: { contains: filter } }, { name: { contains: filter } }],
     };
 
-    const result = await this.prismaService.$transaction([
+    const ORDER_BY_CLAUSE = orderByBuilder<SortUserDto>(sort as SortUserDto);
+
+    const [total, data] = await this.prismaService.$transaction([
       this.prismaService.user.count({
         ...(filter && {
           where: WHERE_CLAUSE,
@@ -33,12 +36,13 @@ export class UsersService {
         ...(filter && {
           where: WHERE_CLAUSE,
         }),
+        ...ORDER_BY_CLAUSE,
       }),
     ]);
 
     return {
-      total: result[0],
-      data: result[1],
+      total,
+      data,
     };
   };
 }
@@ -47,4 +51,5 @@ export interface IFindAllOptions {
   skip?: number;
   limit: number;
   filter?: string;
+  sort?: Partial<SortUserDto>;
 }
