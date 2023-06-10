fd14dee50e7afe96935899f3647f5e995ebe7c9d
talperets <tal.perets3@gmail.com>
2023-06-10 22:00:21 +0300

    Fixing logo

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Reduced.to',
   tagline: 'Highly Efficient Open Source URL Shortener',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
   url: 'https://reduced.to/',
   baseUrl: '/',  
   projectName: 'reduced.to', 
@@ -32,13 +32,7 @@ const config = {
           editUrl:
             '/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/origranot/reduced.to',
-        },
+        
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
