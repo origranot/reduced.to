8a6413bc4a758bba067f315449767e997ef32993
orig <oriorigranot@gmail.com>
2023-06-15 14:13:00 +0300

    individual throttler

--- backend/src/shared/guards/custom-throttler/custom-throttler.ts
+++ backend/src/shared/guards/custom-throttler/custom-throttler.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { ThrottlerGuard } from '@nestjs/throttler';
+import { AppLoggerSerivce } from '../../../logger/logger.service';
 
 @Injectable()
 export class CustomThrottlerGuard extends ThrottlerGuard {
+  protected getTracker(req: Record<string, any>): string {
+    const ip = req.ips.length ? req.ips[0] : req.ip;
+    console.log(`Throttling request from ${ip}`);
+
+    return req.ips.length ? req.ips[0] : req.ip; // individualize IP extraction
+  }
+
   protected errorMessage = 'You have exceeded the rate limit for accessing this resource. Please try again later.';
 }
