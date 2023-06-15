41cab6d655b894c38c68ed5d7077ce0e63f552ac
orig <oriorigranot@gmail.com>
2023-06-15 14:34:23 +0300

    debug

--- backend/src/shared/guards/custom-throttler/custom-throttler.ts
+++ backend/src/shared/guards/custom-throttler/custom-throttler.ts
@@ -5,8 +5,7 @@ import { AppLoggerSerivce } from '../../../logger/logger.service';
 @Injectable()
 export class CustomThrottlerGuard extends ThrottlerGuard {
   protected getTracker(req: Record<string, any>): string {
-    const ip = req.ips.length ? req.ips[0] : req.ip;
-    console.log(`Throttling request from ${ip}`);
+    console.log(`Throttling request from`, req);
 
     return req.ips.length ? req.ips[0] : req.ip; // individualize IP extraction
   }
