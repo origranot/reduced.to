5d0917e28f84a73792b138903750ba5e11441ea3
orig <oriorigranot@gmail.com>
2023-06-15 15:10:26 +0300

    fix: pass headers when redirect

--- backend/src/shared/guards/custom-throttler/custom-throttler.ts
+++ backend/src/shared/guards/custom-throttler/custom-throttler.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { ThrottlerGuard } from '@nestjs/throttler';
 import { AppLoggerSerivce } from '../../../logger/logger.service';
+import { Request } from 'express';
 
 @Injectable()
 export class CustomThrottlerGuard extends ThrottlerGuard {
-  protected getTracker(req: Record<string, any>): string {
-    console.log(`Throttling request from`, req);
-
-    return req.ips.length ? req.ips[0] : req.ip; // individualize IP extraction
+  protected getTracker(req: Request): string {
+    const ip = req.headers['x-forwarded-for'] || req.ips.length ? req.ips[0] : req.ip;
+    console.log(ip);
+    return req.headers['x-forwarded-for'] || req.ips.length ? req.ips[0] : req.ip;
   }
 
   protected errorMessage = 'You have exceeded the rate limit for accessing this resource. Please try again later.';
