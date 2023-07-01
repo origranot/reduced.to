9f1055c02b5e34e178da4cca5067369b9765c142
orig <oriorigranot@gamil.com>
2023-07-01 20:53:26 +0300

    fix(custom-throttler.ts): refactor getTracker method to use a helper function getIp to improve code readability and maintainability feat(custom-throttler.ts): add support for retrieving client IP from 'x-qwik-city-client-conn-ip' header if available feat(custom-throttler.ts): add support for retrieving client IP from 'x-forwarded-for' header if 'x-qwik-city-client-conn-ip' header is not available feat(custom-throttler.ts): add support for retrieving client IP from req.socket.remoteAddress if both headers are not available
    fix(index.tsx): add support for passing client IP and country headers to the API request
    feat(index.tsx): add 'x-qwik-city-client-conn-ip' header to the API request if clientConn.ip is available
    feat(index.tsx): add 'x-qwik-city-client-conn-country' header to the API request if clientConn.country is available

--- apps/backend/src/shared/guards/custom-throttler/custom-throttler.ts
+++ apps/backend/src/shared/guards/custom-throttler/custom-throttler.ts
@@ -5,10 +5,15 @@ import { Request } from 'express';
 @Injectable()
 export class CustomThrottlerGuard extends ThrottlerGuard {
   protected getTracker(req: Request): string {
-    const ip = (req.headers['x-forwarded-for'] as string) || req.socket.remoteAddress;
-
-    return ip;
+    return this.getIp(req);
   }
 
   protected errorMessage = 'You have exceeded the rate limit for accessing this resource. Please try again later.';
+
+  private getIp(req: Request): string {
+    const ipFromQwik = req.headers['x-qwik-city-client-conn-ip'] as string;
+    const ip = ipFromQwik || (req.headers['x-forwarded-for'] as string) || req.socket.remoteAddress;
+
+    return ip;
+  }
 }
