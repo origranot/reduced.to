7bf895e8b5cc7d8dc098740745712b59c7941428
talperets <tal.perets3@gmail.com>
2023-07-03 14:34:21 +0300

    Fixing Docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -10,8 +10,8 @@ const config = {
   tagline: 'Highly Efficient Open Source URL Shortener',
   favicon: 'img/favicon.png',
   url: 'https://reduced.to/',
-  baseUrl: '/',  
-  projectName: 'reduced.to', 
+  baseUrl: '/',
+  projectName: 'reduced.to',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   i18n: {
@@ -29,10 +29,9 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           routeBasePath: '/',
-          editUrl:
-            '/',
+          editUrl: '/',
         },
-        
+
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -43,43 +42,29 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      
       navbar: {
+        logo: {
+          alt: 'Site Logo',
+          src: `img/logo.svg`,
+          srcDark: `img/logo.svg`,
+          href: '/',
+          target: '_self',
+        },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Reduced.to',
-          },
-          {
             position: 'right',
-              href:"https://github.com/origranot/reduced.to",
-              html: `
+            href: 'https://github.com/origranot/reduced.to',
+            html: `
                 <a  aria-label="GitHub" class="navbar-github-link">
                   <img src="img/github-logo.png" alt="GitHub Logo" class="navbar-github-logo" />
                 </a>
               `,
-            
-            
           },
         ],
-        
       },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          
-        ],
+
         copyright: `Copyright © ${new Date().getFullYear()} Reduced.to, Inc. Built with Docusaurus.`,
       },
       prism: {
