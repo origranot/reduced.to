a713088aa2f1fac504f349b1d55538ddc074bad3
orimiles5 <ori.miles@boyar.org.il>
2023-07-03 20:04:25 +0300

    Use mocked API responses

--- apps/frontend/tests/shorten-url.spec.ts
+++ apps/frontend/tests/shorten-url.spec.ts
@@ -1,11 +1,18 @@
 import { expect, test } from '@playwright/test';
 import { HomePage } from './pages/home-page';
+import { routes } from './constants';
+import { mockResponses } from './mock-data/mock-responses';
+import type { MockResponse, ResponseBody } from './mock-data/types';
 
 test.describe('URL Shortener App - guest mode', () => {
   let homePage: HomePage;
 
-  // Enter a URL before testing the output options
+  // Enter a URL to shorten before testing the output options
   test.beforeEach(async ({ page }) => {
+    await page.route(routes.SHORTEN_URL, (route) => {
+      route.fulfill(mockResponses.SHORTEN_URL);
+    });
+
     homePage = new HomePage(page);
 
     // Go to home page
@@ -18,26 +25,35 @@ test.describe('URL Shortener App - guest mode', () => {
 
     // Click the `SHORTEN URL` button
     await homePage.shortenUrl();
+
+    const response = await page.waitForResponse(routes.SHORTEN_URL);
+    const responseBody = await response.json();
+
+    expect(response.status()).toBe(201); // Assert status code
+    expect(responseBody.newUrl).toBe(JSON.parse(mockResponses.SHORTEN_URL.body)?.newUrl); // Assert `newUrl` value
   });
 
-  // test('Copy the shortened URL', async ({ page }) => {
-  //   // Assert that the shortened URL is displayed correctly
-  //   const shortenedUrl = await homePage.getShortenedUrl();
-  //   expect(shortenedUrl).toMatch(/^https:\/\/reduced.to\/.*$/);
+  test('Copy the shortened URL', async ({ page }) => {
+    // Assert that the shortened URL is displayed correctly
+    const shortenedUrl = await homePage.getShortenedUrl();
+    // expect(shortenedUrl).toMatch(/^https:\/\/reduced.to\/.*$/);
 
-  //   // Click the copy button
-  //   await homePage.copyUrl();
+    // Click the copy button
+    await homePage.copyUrl();
 
-  //   // Assert that the URL has been copied
-  //   const clipboardData = await page.evaluate(() => navigator.clipboard.readText());
-  //   expect(clipboardData).toBe(shortenedUrl);
-  // });
+    // Assert that the URL has been copied
+    const clipboardData = await page.evaluate(() => navigator.clipboard.readText());
+    expect(clipboardData).toBe(shortenedUrl);
+  });
 
   test('Open shortened URL in a new tab', async ({ page }) => {
     // Assert that the correct URL is opened in a new tab
     const [shortenedLinkTab] = await Promise.all([page.waitForEvent('popup'), homePage.openLinkInNewTab()]);
     await shortenedLinkTab.waitForLoadState('networkidle');
-    expect(shortenedLinkTab.url()).toContain('google.com');
+
+    // Check if a new tab has been opened
+    const isOpened = await shortenedLinkTab.evaluate(() => !!window.opener);
+    expect(isOpened).toBeTruthy(); // Expect a new tab to be opened
 
     // Close the new tab
     await shortenedLinkTab.close();
