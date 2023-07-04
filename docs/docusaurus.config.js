367ffc33ea95cdaa4c774fbda4e63b35acf06383
Tal Perets <112990364+talperets@users.noreply.github.com>
2023-07-04 09:10:56 +0300

    Creating initial docs (#404)
    * Creating initial docs
    
    * Updating links
    
    * Fixing CSS
    
    * deleting old files
    
    * a
    
    * Uploading new docs
    
    * changing to TS
    
    * Fixing logo
    
    * Fixing theme and adding github logo
    
    * Fixing Docs
    
    * fixing linter
    
    * fixing linting issues
    
    * keeping scripts
    
    ---------
    
    Co-authored-by: orig <48911144+origranot@users.noreply.github.com>

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,77 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Reduced.to',
+  tagline: 'Highly Efficient Open Source URL Shortener',
+  favicon: 'img/favicon.png',
+  url: 'https://reduced.to/',
+  baseUrl: '/',
+  projectName: 'reduced.to',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          routeBasePath: '/',
+          editUrl: '/',
+        },
+
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        logo: {
+          alt: 'Site Logo',
+          src: `img/logo.svg`,
+          srcDark: `img/logo.svg`,
+          href: '/',
+          target: '_self',
+        },
+        items: [
+          {
+            position: 'right',
+            href: 'https://github.com/origranot/reduced.to',
+            html: `
+                <a  aria-label="GitHub" class="navbar-github-link">
+                  <img src="img/github-logo.png" alt="GitHub Logo" class="navbar-github-logo" />
+                </a>
+              `,
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+
+        copyright: `Copyright © ${new Date().getFullYear()} Reduced.to, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
