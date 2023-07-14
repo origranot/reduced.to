0d36d1d2428a821202cf6f276c87cce2fd7682a3
orimiles5 <ori.miles@boyar.org.il>
2023-07-14 22:14:35 +0300

    Change file name

--- e2e/tests/pages/authentication/register.page.ts
+++ e2e/tests/pages/authentication/register.page.ts
@@ -6,13 +6,17 @@ export class RegisterPage extends BaseAuthPage {
   readonly submitButton: Locator;
   readonly heading: Locator;
   readonly passwordVisibilityToggler: Locator;
+  readonly verificationAlert = this.page
+    .locator('div')
+    .filter({ hasText: 'Your account is not verified, you may not be able to access all features of the ' })
+    .first();
 
   constructor(page: Page) {
     super(page, 'register');
     this.nameInput = this.page.locator('input[name="displayName"]');
     this.submitButton = this.page.locator('button', { hasText: 'Continue' });
     this.heading = this.page.locator('h1', { hasText: 'Create an account' });
-    this.passwordVisibilityToggler = this.page.locator('input[name="password"] > span');
+    this.passwordVisibilityToggler = this.page.getByRole('main').getByRole('img');
   }
 
   async fillName(name: string) {
