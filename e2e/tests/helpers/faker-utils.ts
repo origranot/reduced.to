04f0e5d6e9ef495f83471b619366ab15380eb6cc
orimiles5 <ori.miles@boyar.org.il>
2023-07-14 23:21:17 +0300

    Add utils

--- e2e/tests/helpers/faker-utils.ts
+++ e2e/tests/helpers/faker-utils.ts
@@ -3,7 +3,9 @@ import { faker } from '@faker-js/faker';
 // Names generators
 export const generateValidName = () => faker.string.sample(8);
 
-export const generateInvalidName = () => faker.string.sample(2);
+export const generateShortName = () => faker.string.sample(2);
+
+export const generateLongName = () => faker.string.sample(26);
 
 // Email generators
 export const generateValidEmail = () => faker.internet.email();
@@ -14,3 +16,8 @@ export const generateInvalidEmail = () => faker.string.sample(10);
 export const generateValidPassword = () => `a${faker.internet.password({ length: 8 })}1`;
 
 export const generateShortPassword = () => faker.internet.password({ length: 5 });
+
+// URL generator
+export const generateValidURL = () => faker.internet.url();
+
+export const generateInvalidURL = () => faker.internet.domainWord();
