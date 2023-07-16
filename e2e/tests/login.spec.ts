e15c2e15f05634bdc28b703f1902f5f8cc4cf125
orimiles5 <ori.miles@boyar.org.il>
2023-07-16 15:14:23 +0300

    Split tests

--- e2e/tests/login.spec.ts
+++ e2e/tests/login.spec.ts
@@ -1,59 +1,87 @@
 import { expect, test } from './fixtures/auth.fixture';
 import { generateInvalidEmail, generateShortPassword, generateValidEmail, generateValidPassword } from './helpers/faker-utils';
 
-test.describe('User login', async () => {
-  test('Heading check', async ({ loginPage }) => {
+test.describe('Login', async () => {
+  let baseUrl: string;
+
+  test.beforeAll(async ({ baseURL }) => {
+    baseUrl = baseURL ?? 'http://localhost:5173/';
+  });
+
+  test('Login page should display a heading', async ({ loginPage }) => {
     await expect(loginPage.heading).toBeVisible();
   });
 
-  test('Successful user login', async ({ page, loginPage, account, baseURL }) => {
-    await loginPage.fillEmail(account.email);
-    await loginPage.fillPassword(account.password);
+  test.describe('Logged in user', () => {
+    test('Should redirect to home page after logging in', async ({ page, loginPage, account }) => {
+      await loginPage.fillEmail(account.email);
+      await loginPage.fillPassword(account.password);
+      await loginPage.submit();
+      await page.waitForURL(baseUrl);
+      expect(page).toHaveURL(baseUrl);
+    });
 
-    await loginPage.submit();
-    await page.waitForURL(/^(?!.*login).*$/);
+    test('Should get cookies after logging in', async ({ authenticatedPage, context }) => {
+      let newCookies = await context.cookies();
+      expect(newCookies.length).toBeGreaterThanOrEqual(2);
+    });
 
-    // Should redirect to home page after logged in
-    expect(page.url()).toBe(baseURL || 'http://localhost:5173/');
+    test('Should display avatar and hide login button after logging in', async ({ authenticatedPage }) => {
+      await expect(authenticatedPage.navbar.avatar).toBeVisible();
+      await expect(authenticatedPage.navbar.loginButton).not.toBeVisible();
+    });
 
-    // Avatar should be visible
-    await expect(loginPage.navbar.avatar).toBeVisible();
-    await expect(loginPage.navbar.loginButton).not.toBeVisible();
+    test('Should display menu buttons after opening the menu', async ({ authenticatedPage }) => {
+      await authenticatedPage.navbar.avatar.click();
+      await expect(authenticatedPage.navbar.logoutButton).toBeVisible();
+      await expect(authenticatedPage.navbar.dashboardButton).toBeVisible();
+    });
+
+    test("Should be able to navigate to user's dashboard", async ({ page, authenticatedPage }) => {
+      await authenticatedPage.navbar.avatar.click();
+      await authenticatedPage.navbar.gotoDashboard();
+      await page.waitForURL(`${baseUrl}dashboard/`);
+      expect(page).toHaveURL(`${baseUrl}dashboard/`);
+    });
+
+    test('Log out', async ({ page, authenticatedPage, context }) => {
+      await authenticatedPage.navbar.avatar.click();
+      await authenticatedPage.navbar.logout();
+      await page.waitForURL(baseUrl);
+
+      // Should redirect to the home page
+      expect(page).toHaveURL(baseUrl);
+
+      // Should display login button and hide avatar
+      await expect(authenticatedPage.navbar.loginButton).toBeVisible();
+      await expect(authenticatedPage.navbar.avatar).not.toBeVisible();
+
+      // Should remove cookies
+      let newCookies = await context.cookies();
+      expect(newCookies.length).toBeLessThanOrEqual(0);
+    });
   });
 
-  test('Invalid email or password', async ({ page, loginPage }) => {
-    // Fill in random credentials
-    await loginPage.fillEmail(generateValidEmail());
-    await loginPage.fillPassword(generateValidPassword());
+  test.describe('Invalid credentials', async () => {
+    test('Invalid email or password', async ({ page, loginPage }) => {
+      await loginPage.fillEmail(generateValidEmail());
+      await loginPage.fillPassword(generateValidPassword());
+      await loginPage.submit();
+      await expect(page.getByText('Invalid email or password')).toBeVisible();
+    });
 
-    await loginPage.submit();
+    test('Invalid email', async ({ page, loginPage }) => {
+      await loginPage.fillEmail(generateInvalidEmail());
+      await loginPage.fillPassword(generateValidPassword());
+      await loginPage.submit();
+      await expect(page.getByText('Please enter a valid email')).toBeVisible();
+    });
 
-    // Should show an error message
-    const errorMsg = page.getByText('Invalid email or password');
-    await expect(errorMsg).toBeVisible();
-  });
-
-  test('Invalid email', async ({ page, loginPage }) => {
-    // Fill in a random string
-    await loginPage.fillEmail(generateInvalidEmail());
-    await loginPage.fillPassword(generateValidPassword());
-
-    await loginPage.submit();
-
-    // Should show an error message
-    const errorMsg = page.getByText('Please enter a valid email');
-    await expect(errorMsg).toBeVisible();
-  });
-
-  test('Short password', async ({ page, loginPage }) => {
-    await loginPage.fillEmail(generateValidEmail());
-    // Fill in short password
-    await loginPage.fillPassword(generateShortPassword());
-
-    await loginPage.submit();
-
-    // Should show an error message
-    const errorMsg = page.getByText('Password must be at least 6 characters');
-    await expect(errorMsg).toBeVisible();
+    test('Short password', async ({ page, loginPage }) => {
+      await loginPage.fillEmail(generateValidEmail());
+      await loginPage.fillPassword(generateShortPassword());
+      await loginPage.submit();
+      await expect(page.getByText('Password must be at least 6 characters')).toBeVisible();
+    });
   });
 });
