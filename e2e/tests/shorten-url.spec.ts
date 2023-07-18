02a235788eed38328c5ded626ebfc3063b4d8474
orimiles5 <ori.miles@boyar.org.il>
2023-07-18 22:29:49 +0300

    Remove redundant comments

--- e2e/tests/shorten-url.spec.ts
+++ e2e/tests/shorten-url.spec.ts
@@ -1,13 +1,10 @@
-import { expect, test } from './fixtures/main.fixture';
+import { expect, test } from './fixtures';
 import { routes } from './helpers/constants';
-import { generateInvalidURL, generateValidURL } from './helpers/faker-utils';
+import { invalidURL, validURL } from './helpers/faker-utils';
 
 test.describe('URL Shortener App', () => {
   test.beforeEach(async ({ page, mainPage }) => {
-    // Enter the URL in the input field
-    await mainPage.fillUrl(generateValidURL());
-
-    // Click the `SHORTEN URL` button
+    await mainPage.fillUrl(validURL());
     await mainPage.shorten();
 
     const response = await page.waitForResponse(routes.SHORTEN_URL);
@@ -18,9 +15,7 @@ test.describe('URL Shortener App', () => {
   });
 
   test('Copy the shortened URL', async ({ page, mainPage }) => {
-    // Assert that the shortened URL is displayed correctly
     await expect(mainPage.shortenedLink).toBeVisible();
-
     await mainPage.copyUrl();
 
     const shortenedUrl = await mainPage.getShortenedUrl();
@@ -31,45 +26,35 @@ test.describe('URL Shortener App', () => {
   });
 
   test('Open shortened URL in a new tab', async ({ page, mainPage, context }) => {
-    // Assert that the correct URL is opened in a new tab
     const [shortenedLinkTab] = await Promise.all([page.waitForEvent('popup'), mainPage.openLinkInNewTab()]);
     await shortenedLinkTab.waitForLoadState('networkidle');
-
-    // Check if a new tab has been opened
     const pages = context.pages();
-
     expect(pages.length).toBeGreaterThan(1);
   });
 
-  test('Generate shortened URL QR code', async ({ mainPage }) => {
-    // Click the `QR code` button
+  test('Show shortened URL QR code', async ({ mainPage }) => {
     await mainPage.showQRCode();
-
-    // Assert that the QR code is displayed
     await expect(mainPage.qrCode).toBeVisible();
   });
 
   test('Share shortened URL on Twitter', async ({ page, mainPage }) => {
     const shareOnTwitterLinkPattern = new RegExp(/https:\/\/twitter\.com\/.*\/share\?text=[^&]+&url=[^&]+&hashtags=[^&]+/);
     const [twitterTab] = await Promise.all([page.waitForEvent('popup'), mainPage.shareOnTwitter()]);
-
     const decodedUrl = decodeURIComponent(twitterTab.url());
-
-    // Assert that the correct URL is opened in a new tab
     expect(decodedUrl).toMatch(shareOnTwitterLinkPattern);
   });
 });
 
 test.describe('Invalid input URL', () => {
   test('Should raise an error', async ({ page, mainPage }) => {
-    await mainPage.fillUrl(generateInvalidURL());
+    await mainPage.fillUrl(invalidURL());
     await mainPage.shorten();
 
     const response = await page.waitForResponse(routes.SHORTEN_URL);
     const jsonResponse = await response.json();
 
-    expect(response.status()).toBe(400); // Assert status code
-    expect(jsonResponse.newUrl).toBeUndefined(); // Assert `newUrl` value
-    await expect(mainPage.errorMsg).toBeVisible(); // Assert that an error is displayed
+    expect(response.status()).toBe(400);
+    expect(jsonResponse.newUrl).toBeUndefined();
+    await expect(mainPage.errorMsg).toBeVisible();
   });
 });
