928e657f6e3ebeb8d1ce375febc0cb53c4ef84a6
orig <oriorigranot@gamil.com>
2023-09-11 22:35:49 +0300

    fix: remove radius from button

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -7,12 +7,6 @@ import { qwikNxVite } from 'qwik-nx/plugins';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
-  console.log('env', {
-    DOMAIN: env.DOMAIN,
-    CLIENTSIDE_API_DOMAIN: env.CLIENTSIDE_API_DOMAIN,
-    API_DOMAIN: env.API_DOMAIN,
-  });
-
   return {
     cacheDir: '../../node_modules/.vite/apps/frontend',
     plugins: [
