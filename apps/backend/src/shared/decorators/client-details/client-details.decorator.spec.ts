1e8914c749a7b257226d531a84c61cd71dda28c0
orig <48911144+origranot@users.noreply.github.com>
2023-09-29 16:43:52 +0300

    feat: add user-agent to stats station (#462)
    * feat: add user-agent to stats station
    
    * fix: lint issues
    
    * fix: fix backend tests
    
    ---------
    
    Co-authored-by: orig <oriorigraont@gmail.com>

---
+++ apps/backend/src/shared/decorators/client-details/client-details.decorator.spec.ts
@@ -0,0 +1,88 @@
+import { ExecutionContext } from '@nestjs/common';
+import { ClientDetails, IClientDetails } from './client-details.decorator';
+import { ROUTE_ARGS_METADATA } from '@nestjs/common/constants';
+
+describe('ClientDetails Decorator', () => {
+  const getParamDecoratorFactory = () => {
+    class TestDecorator {
+      public test(@ClientDetails() _value: IClientDetails) {
+        // Empty function for testing purposes
+      }
+    }
+
+    const args = Reflect.getMetadata(ROUTE_ARGS_METADATA, TestDecorator, 'test');
+    return args[Object.keys(args)[0]].factory;
+  };
+
+  const handleDecorator = getParamDecoratorFactory();
+
+  const getMockContext = (mockRequest: object) => {
+    return {
+      switchToHttp: () => ({
+        getRequest: () => mockRequest,
+      }),
+    } as ExecutionContext;
+  };
+
+  it('should extract client details from the request', () => {
+    const IP = '192.168.1.1';
+    const USER_AGENT = 'Test User Agent';
+
+    const mockContext = getMockContext({
+      headers: {
+        'x-forwarded-for': IP,
+        'user-agent': USER_AGENT,
+      },
+      socket: {
+        remoteAddress: '192.168.1.2',
+      },
+    });
+
+    const result = handleDecorator(null, mockContext);
+
+    expect(result).toEqual({
+      ip: IP,
+      userAgent: USER_AGENT,
+    });
+  });
+
+  it('should extract client details from the request when x-forwarded-for is not present', () => {
+    const IP = '192.168.1.1';
+    const USER_AGENT = 'Test User Agent';
+
+    const mockContext = getMockContext({
+      headers: {
+        'user-agent': USER_AGENT,
+      },
+      socket: {
+        remoteAddress: IP,
+      },
+    });
+
+    const result = handleDecorator(null, mockContext);
+
+    expect(result).toEqual({
+      ip: IP,
+      userAgent: USER_AGENT,
+    });
+  });
+
+  it('should extract client details from the request when user-agent is not present', () => {
+    const IP = '192.168.1.1';
+    const USER_AGENT = undefined;
+
+    const mockContext = getMockContext({
+      headers: {},
+      socket: {
+        remoteAddress: IP,
+      },
+    });
+
+    const result = handleDecorator(null, mockContext);
+
+    expect(result).toEqual({
+      ip: IP,
+      userAgent: USER_AGENT,
+    });
+  });
+});
