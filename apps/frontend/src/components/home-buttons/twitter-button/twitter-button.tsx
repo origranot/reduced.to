a2171c8b8858d79375d1705787f3698a09b0f277
Woon Koh <woonsta13@gmail.com>
2023-10-01 22:24:10 -0700

    updated so twitter logo color matches other icons

--- apps/frontend/src/components/home-buttons/twitter-button/twitter-button.tsx
+++ apps/frontend/src/components/home-buttons/twitter-button/twitter-button.tsx
@@ -15,8 +15,11 @@ export const TwitterButton = component$((props: TwitterButtonProps) => {
       onClick$={props.handleClick$}
     >
       {/* New logo for twitter */}
-      <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-white" viewBox="0 0 512 512">
-        <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+      <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 512 512">
+        <path
+          fill="currentColor"
+          d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
+        />
       </svg>
     </button>
   );
