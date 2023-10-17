8a46de87c561e5d9a8d8a21945a2f7ecbdbf5bbe
orig <48911144+origranot@users.noreply.github.com>
2023-10-17 22:48:51 +0300

    feat: add resources dropdown (#522)
    Co-authored-by: orig <oriorigranot@gamil.com>

---
+++ apps/frontend/src/components/navbar/resources/resources.tsx
@@ -0,0 +1,23 @@
+import { component$ } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
+
+export const Resources = component$(() => {
+  return (
+    <div class="dropdown dropdown-hover">
+      <label tabIndex={0} class="btn btn-ghost btn-sm">
+        Resources
+      </label>
+      <ul tabIndex={0} class="p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-48">
+        <li>
+          <a href="https://docs.reduced.to">Docs</a>
+        </li>
+        <li>
+          <Link class="btn-ghost py-2 text-sm justify-between cursor-not-allowed">
+            Report
+            <span class="badge">Soon</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+});
