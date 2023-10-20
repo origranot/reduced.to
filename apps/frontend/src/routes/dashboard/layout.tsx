4e5162b95d0cc4ed8c25ed082380a24b4031fb57
orig <oriorigraont@gmail.com>
2023-10-20 21:36:05 +0300

    feat: add admin menu

--- apps/frontend/src/routes/dashboard/layout.tsx
+++ apps/frontend/src/routes/dashboard/layout.tsx
@@ -12,7 +12,6 @@ export const onGet: RequestHandler = async ({ cookie, redirect }) => {
 
 export default component$(() => {
   const location = useLocation();
-  const currentPath = location.url.pathname.slice(0, -1);
   const user = useGetCurrentUser();
 
   const isDrawerOpen = useSignal(false);
@@ -28,7 +27,11 @@ export default component$(() => {
         <label for="drawer" class="drawer-overlay"></label>
         <ul class="menu p-4 w-64 text-base-content border-r bg-base-100 dark:border-gray-700 block h-full pt-[64px] lg:pt-[0.5px]">
           <li class="py-2 mt-2">
-            <Link href="/dashboard" class={`${currentPath === '/dashboard' ? 'active' : ''}`} onClick$={toggleDrawer}>
+            <Link
+              href="/dashboard"
+              class={`${location.url.pathname.slice(0, -1) === '/dashboard' ? 'active' : ''}`}
+              onClick$={toggleDrawer}
+            >
               <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M19 11H5M19 11C20.1046 11 21 11.8954 21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V13C3 11.8954 3.89543 11 5 11M19 11V9C19 7.89543 18.1046 7 17 7M5 11V9C5 7.89543 5.89543 7 7 7M7 7V5C7 3.89543 7.89543 3 9 3H15C16.1046 3 17 3.89543 17 5V7M7 7H17"
@@ -42,7 +45,7 @@ export default component$(() => {
             </Link>
           </li>
           <li class="py-2 mt-2">
-            <Link class={`${currentPath === '/dashboard/settings' ? 'active' : ''} !cursor-not-allowed`}>
+            <Link class={`${location.url.pathname.slice(0, -1) === '/dashboard/settings' ? 'active' : ''} !cursor-not-allowed`}>
               <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M10.3246 4.31731C10.751 2.5609 13.249 2.5609 13.6754 4.31731C13.9508 5.45193 15.2507 5.99038 16.2478 5.38285C17.7913 4.44239 19.5576 6.2087 18.6172 7.75218C18.0096 8.74925 18.5481 10.0492 19.6827 10.3246C21.4391 10.751 21.4391 13.249 19.6827 13.6754C18.5481 13.9508 18.0096 15.2507 18.6172 16.2478C19.5576 17.7913 17.7913 19.5576 16.2478 18.6172C15.2507 18.0096 13.9508 18.5481 13.6754 19.6827C13.249 21.4391 10.751 21.4391 10.3246 19.6827C10.0492 18.5481 8.74926 18.0096 7.75219 18.6172C6.2087 19.5576 4.44239 17.7913 5.38285 16.2478C5.99038 15.2507 5.45193 13.9508 4.31731 13.6754C2.5609 13.249 2.5609 10.751 4.31731 10.3246C5.45193 10.0492 5.99037 8.74926 5.38285 7.75218C4.44239 6.2087 6.2087 4.44239 7.75219 5.38285C8.74926 5.99037 10.0492 5.45193 10.3246 4.31731Z"
@@ -66,24 +69,74 @@ export default component$(() => {
           {user.value?.role === Role.ADMIN && (
             <>
               <div class="divider"></div>
-              <li class="py-2 mt-2">
-                <Link href="/dashboard/admin" class={`${currentPath === '/dashboard/admin' ? 'active' : ''}`} onClick$={toggleDrawer}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="w-6 h-6"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
-                    />
-                  </svg>
-                  <span class="font-medium">Admin Panel</span>
-                </Link>
+              <li>
+                <details open={true}>
+                  <summary class="font-medium">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+                      />
+                    </svg>
+                    Admin Panel
+                  </summary>
+                  <ul>
+                    <li>
+                      <Link
+                        class={`${location.url.pathname.slice(0, -1) === '/dashboard/admin/analytics' ? 'active' : ''} !cursor-not-allowed`}
+                        onClick$={toggleDrawer}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          class="w-6 h-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605"
+                          />
+                        </svg>
+                        <span class="font-medium">Analytics</span>
+                        <span class="badge badge-neutral">Soon</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        class={`${location.url.pathname.slice(0, -1) === '/dashboard/admin/users' ? 'active' : ''} !cursor-not-allowed`}
+                        onClick$={toggleDrawer}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          class="w-6 h-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+                          />
+                        </svg>
+                        <span class="font-medium">Users</span>
+                        <span class="badge badge-neutral">Soon</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </details>
               </li>
             </>
           )}
