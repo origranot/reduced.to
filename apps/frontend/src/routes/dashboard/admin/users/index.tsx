16e508022d53c87adf95252aeeb03fe58d22cd46
orig <oriorigranot@gamil.com>
2023-10-28 20:25:36 +0300

    feat: add document head for the users route

--- apps/frontend/src/routes/dashboard/admin/users/index.tsx
+++ apps/frontend/src/routes/dashboard/admin/users/index.tsx
@@ -1,5 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import { Columns, TableServerPagination } from '../../../../components/table/table-server-pagination';
+import { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   const columns: Columns = {
@@ -14,3 +15,45 @@ export default component$(() => {
     </>
   );
 });
+
+export const head: DocumentHead = {
+  title: 'Reduced.to | Admin Dashboard - Users',
+  meta: [
+    {
+      name: 'title',
+      content: 'Reduced.to | Admin Dashboard - Users',
+    },
+    {
+      name: 'description',
+      content: 'Reduced.to | Admin Dashboard - See all users',
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      property: 'og:url',
+      content: 'https://reduced.to/dashboard',
+    },
+    {
+      property: 'og:title',
+      content: 'Reduced.to | Admin Dashboard - Users',
+    },
+    {
+      property: 'og:description',
+      content: 'Reduced.to | Admin Dashboard - See all users',
+    },
+    {
+      property: 'twitter:card',
+      content: 'summary',
+    },
+    {
+      property: 'twitter:title',
+      content: 'Reduced.to | Admin Dashboard - Users',
+    },
+    {
+      property: 'twitter:description',
+      content: 'Reduced.to | Admin Dashboard - See all users',
+    },
+  ],
+};
