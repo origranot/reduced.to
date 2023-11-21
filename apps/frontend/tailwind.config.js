74dad3d1071225f2793349416e264370b70d033e
orig <oriorigranot@gamil.com>
2023-11-21 13:57:21 +0200

    fix: prettier

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -23,37 +23,40 @@ module.exports = {
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
-    themes: [{
-      'winter': {
-        "color-scheme": "light",
-        "primary": "oklch(56.86% 0.255 257.57)",
-        "secondary": "#cfe1ff",
-        "accent": "#C148AC",
-        "neutral": "#021431",
-        "base-100": "#ffffff",
-        "base-200": "#F2F7FF",
-        "base-300": "#E3E9F4",
-        "base-content": "#394E6A",
-        "info": "#93E7FB",
-        "success": "#81CFD1",
-        "warning": "#EFD7BB",
-        "error": "#E58B8B",
-      }
-    }, {
-      'dracula': {
-        "color-scheme": "dark",
-        "primary": "#7c3aed",
-        "secondary": "#4b3eab",
-        "accent": "#ffb86c",
-        "neutral": "#414558",
-        "base-100": "#0F172A",
-        "base-200": '#1E293B',
-        "base-content": "#f8f8f2",
-        "info": "#8be9fd",
-        "success": "#50fa7b",
-        "warning": "#f1fa8c",
-        "error": "#ff5555",
-      }
-    }],
+    themes: [
+      {
+        winter: {
+          'color-scheme': 'light',
+          primary: 'oklch(56.86% 0.255 257.57)',
+          secondary: '#cfe1ff',
+          accent: '#C148AC',
+          neutral: '#021431',
+          'base-100': '#ffffff',
+          'base-200': '#F2F7FF',
+          'base-300': '#E3E9F4',
+          'base-content': '#394E6A',
+          info: '#93E7FB',
+          success: '#81CFD1',
+          warning: '#EFD7BB',
+          error: '#E58B8B',
+        },
+      },
+      {
+        dracula: {
+          'color-scheme': 'dark',
+          primary: '#7c3aed',
+          secondary: '#4b3eab',
+          accent: '#ffb86c',
+          neutral: '#414558',
+          'base-100': '#0F172A',
+          'base-200': '#1E293B',
+          'base-content': '#f8f8f2',
+          info: '#8be9fd',
+          success: '#50fa7b',
+          warning: '#f1fa8c',
+          error: '#ff5555',
+        },
+      },
+    ],
   },
 };
