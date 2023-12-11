d92def46a05c4aa8c668813bc5d7c709d90ae92c
orig <oriorigranot@gamil.com>
2023-12-11 15:52:21 +0200

    feat: created dockerfile for tracker ms

--- apps/tracker/src/stats/stats.consumer.ts
+++ apps/tracker/src/stats/stats.consumer.ts
@@ -1,13 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { ConsumerService } from '@reduced.to/queue-manager';
 import { AppConfigService } from '@reduced.to/config';
+import { AppLoggerSerivce } from '@reduced.to/logger';
+import { StatsService } from './stats.service';
+import * as geoip from 'geoip-lite';
+import { createHash } from 'node:crypto';
+import { Message } from 'memphis-dev/*';
 
 @Injectable()
 export class StatsConsumer extends ConsumerService {
-  constructor(config: AppConfigService) {
+  constructor(config: AppConfigService, private readonly loggerService: AppLoggerSerivce, private readonly statsService: StatsService) {
     super('tracker', config.getConfig().tracker.stats.queueName);
   }
-  async onMessage(message: any): Promise<void> {
-    console.log('StatsConsumer', message);
+
+  async onMessage(message: Message): Promise<void> {
+    const { ip, userAgent, key, url } = message.getDataAsJson() as { ip: string; userAgent: string; key: string; url: string };
+    message.ack();
+
+    const hashedIp = createHash('sha256').update(ip).digest('hex');
+    const isUniqueVisit = await this.statsService.isUniqueVisit(key, hashedIp);
+
+    if (!isUniqueVisit) {
+      return;
+    }
+
+    const geo = geoip.lookup(ip);
+    await this.statsService.addVisit(key, {
+      hashedIp,
+      ua: userAgent,
+      geo,
+    });
+
+    this.loggerService.log(`Added unique visit for ${key}`);
   }
 }
--- apps/tracker/src/stats/stats.module.ts
+++ apps/tracker/src/stats/stats.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { StatsService } from './stats.service';
 import { StatsConsumer } from './stats.consumer';
 import { QueueManagerModule, QueueManagerService } from '@reduced.to/queue-manager';
+import { PrismaModule } from '@reduced.to/prisma';
 
 @Module({
-  imports: [QueueManagerModule],
+  imports: [PrismaModule, QueueManagerModule],
   providers: [StatsService, StatsConsumer, QueueManagerService],
   exports: [StatsService],
 })
