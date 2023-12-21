59ef243017e2f89dff8ab3125a2d49348475aed6
orig <oriorigranot@gamil.com>
2023-12-21 23:43:16 +0200

    feat: added more style

--- apps/frontend/src/routes/dashboard/settings/index.tsx
+++ apps/frontend/src/routes/dashboard/settings/index.tsx
@@ -12,73 +12,66 @@ export default component$(() => {
   });
 
   return (
-    <div class="shadow-[0_8px_30px_rgb(0,0,0,0.12)] rounded-xl w-full p-5">
+    <div class="shadow-[0_8px_30px_rgb(0,0,0,0.12)] rounded-xl w-full p-5 text-left">
       <Form>
-        <div class="grid sm:grid-cols-12 gap-2 sm:gap-6">
-          <div class="sm:col-span-3">
-            <label class="inline-block text-gray-800 mt-2.5 dark:text-gray-200">Profile photo</label>
+        <div class="grid grid-cols-3 gap-4">
+          <div>
+            <div class="font-bold">Personal Information</div>
+            <span class="text-sm text-gray-500">Update your personal information</span>
           </div>
-
-          <div class="sm:col-span-9">
-            <div class="flex items-center gap-5">
-              <img
-                class="inline-block h-16 w-16 rounded-full ring-2 ring-white dark:ring-gray-800"
-                src="../assets/img/160x160/img1.jpg"
-                alt="Image Description"
-              />
-              <div class="flex gap-x-2">
-                <div>
-                  <button
-                    type="button"
-                    class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                  >
-                    <svg
-                      class="flex-shrink-0 w-4 h-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-                      <polyline points="17 8 12 3 7 8" />
-                      <line x1="12" x2="12" y1="3" y2="15" />
-                    </svg>
-                    Upload photo
-                  </button>
+          <div class="col-span-2">
+            <div class="flex gap-8 items-center">
+              <div class="avatar">
+                <div class="w-24 rounded">
+                  <img width="844" height="844" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                 </div>
+              </div>
+              <div class="flex flex-col gap-2">
+                <button class="btn btn-sm btn-natural">Change avatar</button>
+                <span class="text-sm text-gray-500">JPG, JPEG or PNG. 1MB max.</span>
+              </div>
+            </div>
+            <div class="pt-5">
+              <label for="name" class="block text-sm mb-2 dark:text-white text-left">
+                Display name
+              </label>
+              <input type="name" id="name" name="name" class="py-3 px-4 w-full max-w-xs input input-bordered" required />
+            </div>
+            <div class="pt-5">
+              <label for="email" class="block text-sm mb-2 dark:text-white text-left">
+                Email address
+              </label>
+              <div class="flex gap-4 items-center">
+                <input
+                  type="email"
+                  value={'email@gmail.com'}
+                  id="email"
+                  name="email"
+                  class="py-3 px-4 w-full max-w-xs input input-bordered"
+                  disabled
+                />
+                <label class="text-sm text-gray-500">Email is verified</label>
+                {/* <button class="btn btn-warning">Verify now</button> */}
+              </div>
+              <div class="pt-5">
+                <button type="submit" class="btn btn-sm btn-primary">
+                  {action.isRunning ? <span class="loading loading-spinner-small"></span> : 'Save'}
+                </button>
               </div>
             </div>
           </div>
-
-          <div class="sm:col-span-3">
-            <label for="fullName" class="inline-block text-gray-800 mt-2.5 dark:text-gray-200">
-              Full name
-            </label>
-          </div>
-
-          <div class="sm:col-span-4 gap-5">
-            <input id="fullName" type="text" class="py-2 px-3 block w-full input input-bordered" placeholder="Maria" />
-          </div>
-
         </div>
-        <div class="mt-5 flex justify-end gap-x-2">
-          <button
-            type="button"
-            class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-          >
-            Cancel
-          </button>
-          <button
-            type="button"
-            class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-          >
-            Save changes
-          </button>
+        <div class="divider py-3"></div>
+        <div class="grid grid-cols-3 gap-4">
+          <div>
+            <div class="font-bold">Delete account</div>
+            <span class="text-sm text-gray-500">
+              This action is not reversible, all information related to this account will be deleted permanently.
+            </span>
+          </div>
+          <div class="col-span-2">
+            <button class="btn btn-sm btn-error dark:text-gray-200">Delete account</button>
+          </div>
         </div>
       </Form>
     </div>
