4965bcbcf10353b01f788d1f1529b485e6d91750
orig <oriorigranot@gamil.com>
2024-05-19 12:45:17 +0300

    fix: debug

--- apps/backend/src/shared/decorators/client-details/client-details.decorator.ts
+++ apps/backend/src/shared/decorators/client-details/client-details.decorator.ts
@@ -2,6 +2,8 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const ClientDetails = createParamDecorator((data: unknown, ctx: ExecutionContext): IClientDetails => {
   const request = ctx.switchToHttp().getRequest();
+  console.log('HEADERS', request.headers);
+  console.log('GOT IP', request.headers['x-forwarded-for'] || request.socket.remoteAddress);
   const ip = (request.headers['x-forwarded-for'] || request.socket.remoteAddress) as string;
   const userAgent = request.headers['user-agent'];
 
